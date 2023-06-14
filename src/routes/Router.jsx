33b49bc8c3660c76a395cceec1dba37eb66647c9
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-14 14:05:02 +0900

    style: 라우팅 경로 변경 및 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Login from '../pages/Login/Login';
 import Join from '../pages/Join/Join';
-import ProfileSetting from '../pages/Join/ProfileSetting';
-import Home from '../pages/Home';
+import Profile from '../pages/Join/Profile';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
-        <Route path="/join/profile" element={<ProfileSetting />} />
+        <Route path="/join/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default Router;
+
