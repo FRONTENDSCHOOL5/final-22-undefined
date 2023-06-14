3de7cf2e526eb60c30a20c076803b5031f858d57
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 02:46:21 +0900

    feat: 이미지 유효성 검사 및 업로드 구현 (#16)

--- src/components/common/Input/ImgInput.jsx
+++ src/components/common/Input/ImgInput.jsx
@@ -30,13 +30,41 @@ const ProfileImg = styled.img`
 
 const UploadInput = styled.input``;
 
+const ALLOWED_EXTENSIONS = ['.jpg', '.gif', '.png', '.jpeg', '.bmp', '.tif', '.heic'];
+
 const ImgInput = ({ img, setImg }) => {
+  const handleChange = async (event) => {
+    if (!event.target.files) {
+      return;
+    }
+
+    const file = event.target.files[0];
+    const fileExtenstion = file.name.split('.')[1].toLowerCase();
+
+    if (!ALLOWED_EXTENSIONS.includes(`.${fileExtenstion}`)) return;
+
+    const formData = new FormData();
+    formData.append('image', event.target.files[0]);
+
+    try {
+      const response = await fetch('https://api.mandarin.weniv.co.kr/image/uploadfile', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await response.json();
+      setImg(data.filename);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <>
       <Label htmlFor='profileImg'>
-        <ProfileImg src={Ellipse} alt='프로필 이미지' />
+        <ProfileImg src={img === '' ? Ellipse : `https://api.mandarin.weniv.co.kr/${img}`} alt='프로필 이미지' />
       </Label>
-      <UploadInput id='profileImg' type='file' className='a11y-hidden' />
+      <UploadInput id='profileImg' type='file' className='a11y-hidden' onChange={handleChange} accept='image/*' />
     </>
   );
 };
