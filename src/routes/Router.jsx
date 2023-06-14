b2c99965e661b6362714634d81bc0146a6d545cf
Jiyun Park <jiyun991127@gmail.com>
2023-06-14 15:08:28 +0900

    fix: 라우터 경로 변경

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Join from '../pages/Join/Join';
-import ProfileSetting from '../pages/Join/ProfileSetting';
-import Home from '../pages/Home';
+import Profile from '../pages/Join/Profile';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/join" element={<Join />} />
-        <Route path="/join/profile" element={<ProfileSetting />} />
+        <Route path="/join/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
