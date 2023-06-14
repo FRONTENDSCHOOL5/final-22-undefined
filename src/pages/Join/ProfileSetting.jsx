9d66675c5ebc054207efb5f316ae564fb8dc55e3
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 03:14:34 +0900

    feat: form유효성 검사에 따른 버튼 활성화 (#16)

--- src/pages/Join/ProfileSetting.jsx
+++ src/pages/Join/ProfileSetting.jsx
@@ -39,10 +39,16 @@ const ProfileSetting = () => {
     intro: '',
   });
 
+  let isActivated = false;
+  if (error.username === 'noError' && error.accountname === 'noError') isActivated = true;
+
+  // 프로필 수정 페이지에서 버튼이 form 밖에 있어서 onSubmit으로 하지 않고 onClick으로 구현
+  const handleClick = () => {};
+
   return (
     <Main>
       <Wrapper>
-        <Form>
+        <Form onSubmit={(e) => e.preventDefault()}>
           <Title>프로필 설정</Title>
           <Desc>나중에 얼마든지 변경할 수 있습니다.</Desc>
           <ProfileForm
@@ -53,7 +59,7 @@ const ProfileSetting = () => {
             error={error}
             setError={setError}
           />
-          <StartBtn mode='default' size='lg'>
+          <StartBtn mode={isActivated ? 'default' : 'disabled'} size='lg' onClick={handleClick}>
             감귤마켓 시작하기
           </StartBtn>
         </Form>
