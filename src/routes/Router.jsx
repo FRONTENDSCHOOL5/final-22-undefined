d1bfdb478fa1b0865a4d3cb9be966b5b0c4bdaf5
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-14 15:01:46 +0900

    add: Loading 페이지 파일 및 라우팅 경로 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import Login from '../pages/Login/Login';
-import Join from '../pages/Join/Join';
-import Profile from '../pages/Join/Profile';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loading from "../pages/Loading/Loading";
+import Login from "../pages/Login/Login";
+import Join from "../pages/Join/Join";
+import Profile from "../pages/Join/Profile";
+import Home from "../pages/Home/Home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Loading />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/join/profile" element={<Profile />} />
@@ -19,4 +21,3 @@ const Router = () => {
 };
 
 export default Router;
-
