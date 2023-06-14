5b3560f9ffa14681dc19a8764423592acff56995
Jiyun Park <jiyun991127@gmail.com>
2023-06-14 11:25:19 +0900

    feat: 라우팅 경로 설정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Join from '../pages/Join/Join';
+import ProfileSetting from '../pages/Join/ProfileSetting';
+import Home from '../pages/Home';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/join/profile" element={<ProfileSetting />} />
+      </Routes>
     </BrowserRouter>
   );
 };
