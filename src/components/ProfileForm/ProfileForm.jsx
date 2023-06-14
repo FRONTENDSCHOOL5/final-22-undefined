1f893932f5bbac4373e489f32ae0299af0407c74
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 01:37:36 +0900

    refactor: ProfileForm 재사용을 위한 컴포넌트 분리 (#16)

--- src/components/ProfileForm/ProfileForm.jsx
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import ImgInput from '../common/Input/ImgInput';
+import ProfileFormInput from '../common/Input/ProfileFormInput';
 
 const ProfileForm = () => {
-  return <div>ProfileForm</div>;
+  return (
+    <>
+      <ImgInput />
+      <ProfileFormInput
+        id='username'
+        label='사용자 이름'
+        inputProps={{
+          type: 'text',
+          placeholder: '2~10자 이내여야 합니다.',
+        }}
+      />
+      <ProfileFormInput
+        id='accountname'
+        label='계정 ID'
+        inputProps={{
+          type: 'text',
+          placeholder: '영문, 숫자, 특수문자(,),(_)만 사용 가능합니다.',
+        }}
+      />
+      <ProfileFormInput
+        id='intro'
+        label='소개'
+        inputProps={{
+          type: 'text',
+          placeholder: '자신과 판매할 상품에 대하여 소개해주세요',
+        }}
+      />
+    </>
+  );
 };
 
 export default ProfileForm;
