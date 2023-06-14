a235d62b642b3636f4bc99b8b28f44fb58331b34
Jiyun Park <jiyun991127@gmail.com>
2023-06-14 13:13:59 +0900

    design: FormInput 기본 마크업 및 props 설정

--- src/components/common/Input/FormInput.jsx
+++ src/components/common/Input/FormInput.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const FormInput = () => {
-  return <div>FormInput</div>;
+const Container = styled.div`
+  & + & {
+    margin-top: 16px;
+  }
+`;
+
+const Label = styled.label`
+  display: block;
+  color: ${({ theme }) => theme.colors.txtColor};
+  font-size: 12px;
+  margin-bottom: 10px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  font-size: 14px;
+  line-height: 14px;
+  padding-bottom: 8px;
+  border: none;
+  border-bottom: ${({ theme, isInvalid }) =>
+    isInvalid
+      ? `1px solid ${theme.colors.warning}`
+      : `1px solid ${theme.colors.gray}`};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray};
+  }
+
+  &:focus {
+    outline: none;
+    border-bottom: ${({ theme, isInvalid }) =>
+      isInvalid
+        ? `1px solid ${theme.colors.warning}`
+        : `1px solid ${theme.colors.primary}`};
+  }
+`;
+
+const ErrorMessage = styled.span`
+  color: ${({ theme }) => theme.colors.warning};
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+const FormInput = ({ id, label, inputProps }) => {
+  return (
+    <Container>
+      <Label htmlFor={id}>{label}</Label>
+      <StyledInput id={id} {...inputProps} />
+      <ErrorMessage>에러</ErrorMessage>
+    </Container>
+  );
 };
 
 export default FormInput;
