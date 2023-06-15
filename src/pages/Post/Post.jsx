7441a452829abecac09acd90ba8a678d8c3be756
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-15 23:10:00 +0900

    feat: imgName상태에 따른 이미지 태그 src값 추가

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,17 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import uploadIcon from '../../assets/icon/icon-upload.png';
+import Ellipse from '../../assets/Ellipse-1.png';
 
 const ALLOWED_EXTENSIONS = ['.jpg', '.gif', '.png', '.jpeg', '.bmp', '.tif', '.heic'];
-const Label = styled.label`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background: ${({ theme }) => `${theme.colors.primary} url(${uploadIcon}) no-repeat center`};
-`;
 const Post = () => {
   const [imgName, setImgName] = useState('');
 
@@ -22,7 +14,7 @@ const Post = () => {
 
     const file = e.target.files[0];
     console.log(file.name.split('.'));
-    const fileExtenstion = file.name.split('.');
+    const fileExtenstion = file.name.split('.').slice(-1)[0].toLowerCase();
     console.log(fileExtenstion);
     if (!ALLOWED_EXTENSIONS.includes(`.${fileExtenstion}`)) return;
 
@@ -30,12 +22,12 @@ const Post = () => {
     formData.append('image', file);
 
     try {
-      const response = await fetch('https://api.mandarin.weniv.co.kr/image/uploadfile', {
+      const res = await fetch('https://api.mandarin.weniv.co.kr/image/uploadfile', {
         method: 'POST',
         body: formData,
       });
-      const data = await response.json();
-      console.log(data);
+      const data = await res.json();
+      setImgName(data.filename);
     } catch (error) {
       console.log(error.message);
     }
@@ -43,6 +35,11 @@ const Post = () => {
 
   return (
     <>
+      <img
+        style={{ width: '50%' }}
+        src={imgName === '' ? Ellipse : `https://api.mandarin.weniv.co.kr/${imgName}`}
+        alt='게시글 업로드 이미지'
+      />
       <Label htmlFor='postImg'></Label>
       <input className='a11y-hidden' type='file' id='postImg' onChange={handleImgInput} accept='image/*' />
     </>
@@ -50,3 +47,13 @@ const Post = () => {
 };
 
 export default Post;
+
+const Label = styled.label`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: ${({ theme }) => `${theme.colors.primary} url(${uploadIcon}) no-repeat center`};
+`;
