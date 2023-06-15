e6f12d4e9efc4dcbb1ddb9b42ad728a11878f24f
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 13:48:54 +0900

    add: ImgInput styled-components 파일 분리 (#19)

--- src/components/common/Input/ImgInput.jsx
+++ src/components/common/Input/ImgInput.jsx
@@ -1,36 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import uploadIcon from '../../../assets/icon/icon-upload.png';
 import Ellipse from '../../../assets/Ellipse-1.png';
-
-const Label = styled.label`
-  width: 110px;
-  height: 110px;
-  position: relative;
-  display: block;
-  margin: 0 auto 30px;
-  border-radius: 50%;
-  border: ${({ theme }) => `1px solid ${theme.colors.gray}`};
-
-  &::after {
-    content: '';
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    width: 36px;
-    height: 36px;
-    border-radius: 50%;
-    background: ${({ theme }) => `${theme.colors.primary} url(${uploadIcon}) no-repeat center`};
-  }
-`;
-
-const ProfileImg = styled.img`
-  object-fit: cover;
-  height: 100%;
-  border-radius: 50%;
-`;
-
-const UploadInput = styled.input``;
+import * as S from './ImgInput.style';
 
 const ALLOWED_EXTENSIONS = ['.jpg', '.gif', '.png', '.jpeg', '.bmp', '.tif', '.heic'];
 
@@ -64,10 +34,10 @@ const ImgInput = ({ img, setImg }) => {
 
   return (
     <>
-      <Label htmlFor='profileImg'>
-        <ProfileImg src={img === '' ? Ellipse : `https://api.mandarin.weniv.co.kr/${img}`} alt='프로필 이미지' />
-      </Label>
-      <UploadInput id='profileImg' type='file' className='a11y-hidden' onChange={handleChange} accept='image/*' />
+      <S.Label htmlFor='profileImg'>
+        <S.ProfileImg src={img === '' ? Ellipse : `https://api.mandarin.weniv.co.kr/${img}`} alt='프로필 이미지' />
+      </S.Label>
+      <S.UploadInput id='profileImg' type='file' className='a11y-hidden' onChange={handleChange} accept='image/*' />
     </>
   );
 };
