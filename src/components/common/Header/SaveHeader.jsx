83d7fa6bef655cb71b969d6a837face415d22797
Jiyun Park <jiyun991127@gmail.com>
2023-06-16 01:03:38 +0900

    style: 버튼의 mode props 재설정 (#25)

--- src/components/common/Header/SaveHeader.jsx
+++ src/components/common/Header/SaveHeader.jsx
@@ -7,7 +7,7 @@ const SaveHeader = ({ name, mode, ...props }) => {
       <S.BackBtn>
         <span className='a11y-hidden'>뒤로 가기 버튼</span>
       </S.BackBtn>
-      <S.StyledButton mode='default' size='ms' {...props}>
+      <S.StyledButton mode={mode} size='ms' {...props}>
         {name}
       </S.StyledButton>
     </S.Header>
