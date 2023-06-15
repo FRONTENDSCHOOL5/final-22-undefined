52985d9d7a659fb7096fb2baca35af6589f48b24
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 22:07:52 +0900

    design: 공통 컴포넌트인 SaveHeader 기본 마크업 (#25)

--- src/components/common/Header/SaveHeader.jsx
+++ src/components/common/Header/SaveHeader.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import * as S from './SaveHeader.style';
 
-const SaveHeader = () => {
-  return <div>SaveHeader</div>;
+const SaveHeader = ({ name, mode, ...props }) => {
+  return (
+    <S.Header>
+      <S.BackBtn>
+        <span className='a11y-hidden'>뒤로 가기 버튼</span>
+      </S.BackBtn>
+      <S.StyledButton mode='default' size='ms' {...props}>
+        {name}
+      </S.StyledButton>
+    </S.Header>
+  );
 };
 
 export default SaveHeader;
