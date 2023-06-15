ef61c6eec2ad7c735456bd5556ad278dfa01f41e
goyomi <ghdl1004797@gmail.com>
2023-06-15 21:21:39 +0900

    style: home 화면 캐릭터, 문구 추가, 스타일 작업 (#8)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,38 @@ import TopMainNav from './TopMainNav';
 import TabMenu from './TabMenu';
 import SearchBtn from './SearchBtn';
 
+import home from '../../assets/symbol-logo-gray.png';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  text-align: center;
+`;
+const IMG = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+const P = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 14px;
+`;
+
+const Section = styled.section`
+  max-width: 390px;
+  margin: 0 auto;
+`;
+
 const Home = () => {
   return (
-    <>
+    <Section>
       <TopMainNav />
+      <Wrap>
+        <IMG src={home} alt='동그란 말풍선 머리에 귤 꼭지가 붙어있는 감귤 캐릭터' />
+        <p>유저를 검색해 팔로우 해보세요!</p>
+      </Wrap>
       <SearchBtn />
       <TabMenu />
-    </>
+    </Section>
   );
 };
 
