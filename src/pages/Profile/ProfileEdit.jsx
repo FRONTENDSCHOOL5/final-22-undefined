7cc733631655893f766d3a72d0c06b5e668487ea
Jiyun Park <jiyun991127@gmail.com>
2023-06-16 02:45:20 +0900

    add: 유효성 검사를 통한 버튼 활성화 (#24)

--- src/pages/Profile/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit.jsx
@@ -17,32 +17,47 @@ const ProfileEdit = () => {
     username: '',
     intro: '',
   });
+  const [isLoading, setIsLoading] = useState(false);
+
+  // 버튼 활성화
+  let isActivated = false;
+  if (
+    (error.username === '' || error.username === 'noError') &&
+    (error.accountname === '' || error.accountname === 'noError') &&
+    !isLoading
+  ) {
+    isActivated = true;
+  }
 
   useEffect(() => {
     const getInfo = async () => {
       try {
+        setIsLoading(true);
         const response = await fetch('https://api.mandarin.weniv.co.kr/user/myinfo', {
           // token을 그냥 userToken으로 넣어주면 오류 생김 ... 나중에 체크해볼 것
           headers: { Authorization: `Bearer ${JSON.parse(userToken)}` },
         });
 
         const data = await response.json();
-        console.log(data);
         const { username, accountname, intro, image } = data.user;
         setFormData({ accountname, username, intro });
         setImg(image);
+        setIsLoading(false);
       } catch (err) {
         console.log(err);
+        setIsLoading(false);
       }
     };
     getInfo();
-  }, []);
+  }, [userToken]);
+
+  const handleClick = () => {};
 
   return (
     <S.Main>
       <S.LayoutWrapper>
         <S.Title className='a11y-hidden'>프로필 수정 페이지</S.Title>
-        <SaveHeader name='저장' mode='default' />
+        <SaveHeader name='저장' mode={isActivated ? 'default' : 'disabled'} onClick={handleClick} />
         <S.Form>
           <ProfileForm
             formData={formData}
