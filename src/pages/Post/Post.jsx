235cb5dab0efc6b802eb082a3ad691450dd0bff1
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-15 15:52:35 +0900

    feat: 게시글 이미지 업로드 post요청 (#21)

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Post = () => {
-  return <div>Post</div>;
+  const [imgName, setImgName] = useState('');
+
+  const handleImgInput = (e) => {
+    const formData = new FormData();
+    const imgFile = e.target.files[0];
+    console.log(e.target.files);
+    console.log(imgFile);
+    formData.append('image', imgFile);
+
+    try {
+      const response = fetch('https://api.mandarin.weniv.co.kr/', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <>
+      <label htmlFor='postImg'>
+        <img src='https://api.mandarin.weniv.co.kr/*' alt='게시글 업로드 이미지' />
+      </label>
+      <input type='file' id='profileImg' onChange={handleImgInput} accept='image/*' />
+    </>
+  );
 };
 
 export default Post;
