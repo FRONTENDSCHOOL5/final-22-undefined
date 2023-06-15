13a752b945db7b18a666185338b5784ce96c63b8
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 23:44:55 +0900

    design: 프로필 수정 페이지 기본 마크업 (#24)

--- src/pages/Profile/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import SaveHeader from '../../components/common/Header/SaveHeader';
+import * as S from './ProfileEdit.style';
 
 const ProfileEdit = () => {
-  return <div>ProfileEdit</div>;
+  return (
+    <S.Main>
+      <S.LayoutWrapper>
+        <S.Title className='a11y-hidden'>프로필 수정 페이지</S.Title>
+        <SaveHeader name='저장' mode='default' />
+        <S.Form></S.Form>
+      </S.LayoutWrapper>
+    </S.Main>
+  );
 };
 
 export default ProfileEdit;
