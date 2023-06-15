5e2230f5f13eaf684527f616e6b911652e877d6f
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 23:44:55 +0900

    design: 프로필 수정 페이지 기본 마크업 (#24)

---
+++ src/pages/Profile/ProfileEdit.style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Wrapper from '../../components/common/Wrapper/Wrapper';
+
+export const Main = styled.main``;
+
+export const LayoutWrapper = styled(Wrapper)`
+  padding: 0 34px;
+`;
+
+export const Form = styled.form``;
+
+export const Title = styled.h1``;
