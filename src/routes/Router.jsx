0fe79a681a6efe4d3cb0bfab14c499f67c88a1a9
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-15 23:10:23 +0900

    style: router에 pos경로 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,6 +5,7 @@ import Home from '../pages/Home/Home';
 import Join from '../pages/Join/Join';
 import ProfileSetting from '../pages/Join/ProfileSetting';
 import Login from '../pages/Login/Login';
+import Post from '../pages/Post/Post';
 
 const Router = () => {
   return (
@@ -15,6 +16,7 @@ const Router = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/join' element={<Join />} />
         <Route path='/join/profile' element={<ProfileSetting />} />
+        <Route path='/post' element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
