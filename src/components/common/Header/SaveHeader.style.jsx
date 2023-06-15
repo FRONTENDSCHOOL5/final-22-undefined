a079125e0c1d49047bf41362b454ee8925b8915d
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 22:07:52 +0900

    design: 공통 컴포넌트인 SaveHeader 기본 마크업 (#25)

---
+++ src/components/common/Header/SaveHeader.style.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import ArrowLeftIcon from '../../../assets/icon/icon-arrow-left.png';
+import Button from '../Button/Button';
+
+export const Header = styled.header`
+  padding: 0 16px;
+  height: 48px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-bottom: ${({ theme }) => `1px solid ${theme.colors.gray}`};
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BackBtn = styled.button`
+  width: 22px;
+  height: 22px;
+  background: url(${ArrowLeftIcon}) no-repeat center;
+`;
+
+export const StyledButton = styled(Button)`
+  width: 90px;
+`;
