bbac6534e6955afae644877ac7471184328c5288
goyomi <ghdl1004797@gmail.com>
2023-06-15 20:28:15 +0900

    add: tab-menu 파일, 화면 구현 및 버튼 활성화 작업 (close#22)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopMainNav from './TopMainNav';
 import styled from 'styled-components';
+import TabMenu from './TabMenu';
 
 const Home = () => {
   return (
     <>
       <TopMainNav />
+      <TabMenu />
     </>
   );
 };
