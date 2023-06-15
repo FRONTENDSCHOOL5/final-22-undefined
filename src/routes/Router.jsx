5fa65790ffdec0ca8fc60d4aa077c6bb0f428e94
hyeonbin jeong <duafuuu@gmail.com>
2023-06-16 02:01:48 +0900

    chore: 프로필 피드 페이지 경로 추가 #27

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,6 +5,8 @@ import Home from '../pages/Home/Home';
 import Join from '../pages/Join/Join';
 import ProfileSetting from '../pages/Join/ProfileSetting';
 import Login from '../pages/Login/Login';
+import YourProfileFeed from './../pages/Profile/YourProfileFeed';
+import MyProfileFeed from './../pages/Profile/MyProfileFeed';
 
 const Router = () => {
   return (
@@ -15,6 +17,8 @@ const Router = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/join' element={<Join />} />
         <Route path='/join/profile' element={<ProfileSetting />} />
+        <Route path='/yourProfile' element={<YourProfileFeed />} />
+        <Route path='/myProfile' element={<MyProfileFeed />} />
       </Routes>
     </BrowserRouter>
   );
