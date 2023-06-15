162db99097a4bf12db4a8e95b5e0ec7f9d3ab65e
hyeonbin jeong <duafuuu@gmail.com>
2023-06-16 00:06:23 +0900

    style: Wrapper 관련 padding 값 수정

--- src/components/common/Wrapper/Wrapper.jsx
+++ src/components/common/Wrapper/Wrapper.jsx
@@ -4,7 +4,6 @@ const StyledWrapper = styled.div`
   max-width: 390px;
   min-height: 80vh;
   margin: 40px auto;
-  padding: 0 34px;
   box-shadow: ${({ theme }) => `0px 0px 5px ${theme.colors.gray}`};
   border: ${({ theme }) => `1px solid ${theme.colors.gray}`};
 `;
