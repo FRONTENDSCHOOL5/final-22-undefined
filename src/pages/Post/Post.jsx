e2e6ac0ebb8004e933e9759f2d204df3e802492c
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-16 01:33:01 +0900

    feat: 게시글 입력창 생성(#28)

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -5,7 +5,8 @@ import Ellipse from '../../assets/Ellipse-1.png';
 
 const ALLOWED_EXTENSIONS = ['.jpg', '.gif', '.png', '.jpeg', '.bmp', '.tif', '.heic'];
 const Post = () => {
-  const [imgName, setImgName] = useState(null);
+  const [imgName, setImgName] = useState('');
+  const [imageList, setImageList] = useState([]);
 
   const handleImgInput = async (e) => {
     if (!e.target.files || e.target.files.length === 0) {
@@ -27,7 +28,8 @@ const Post = () => {
         body: formData,
       });
       const data = await res.json();
-      setImgName(data.filename);
+      setImgName([...imgName, data.filename]);
+      console.log(data.filename);
     } catch (error) {
       console.log(error.message);
     }
@@ -35,25 +37,49 @@ const Post = () => {
 
   return (
     <>
-      <main>
-        <h2 className='a11y-hidden'>게시글 작성창</h2>
-        {imgName && (
-          <img
-            style={{ width: '20%' }}
-            src={imgName === '' ? Ellipse : `https://api.mandarin.weniv.co.kr/${imgName}`}
-            alt='게시글 업로드 이미지'
-          />
-        )}
-        <Label htmlFor='postImg'></Label>
-        <input className='a11y-hidden' type='file' id='postImg' onChange={handleImgInput} accept='image/*' />
-      </main>
+      <Main>
+        <h2 className='a11y-hidden'>게시글 작성</h2>
+        <UserProfile src={Ellipse} />
+        <PostArticle>
+          <h3 className='a11y-hidden'>게시글 작성 form</h3>
+          <form>
+            <Textarea name='text' id='' cols='30' rows='10'></Textarea>
+
+            <ImgUpload htmlFor='imgUpload'></ImgUpload>
+            <input className='a11y-hidden' type='file' id='imgUpload' onChange={handleImgInput} accept='image/*' />
+          </form>
+        </PostArticle>
+        <section>
+          <h4 className='a11y-hidden'>추가 이미지</h4>
+          <ul>
+            {imgName && (
+              <img
+                style={{ width: '20%' }}
+                src={imgName === '' ? Ellipse : `https://api.mandarin.weniv.co.kr/${imgName}`}
+                alt='게시글 업로드 이미지'
+              />
+            )}
+          </ul>
+        </section>
+      </Main>
     </>
   );
 };
 
 export default Post;
 
-const Label = styled.label`
+const Main = styled.main`
+  padding: 20px 16px;
+`;
+
+const UserProfile = styled.img`
+  width: 42px;
+  height: 42px;
+  object-fit: cover;
+  border-radius: 50%;
+`;
+
+const ImgUpload = styled.label`
   position: absolute;
   right: 0;
   bottom: 0;
@@ -61,4 +87,18 @@ const Label = styled.label`
   height: 50px;
   border-radius: 50%;
   background: ${({ theme }) => `${theme.colors.primary} url(${uploadIcon}) no-repeat center`};
+`;
+
+const Textarea = styled.textarea`
+  display: inline;
+  border: none;
+  resize: none;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const PostArticle = styled.article`
+  width: 100%;
+  display: inline-block;
 `;
