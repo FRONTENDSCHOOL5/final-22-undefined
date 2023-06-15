0ec130d9947b2e2389e3b588c4fcf22114284932
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 23:43:16 +0900

    design: Wrapper 최소 높이값 변경

--- src/components/common/Wrapper/Wrapper.jsx
+++ src/components/common/Wrapper/Wrapper.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   max-width: 390px;
-  min-height: 80vh;
+  min-height: 820px;
   margin: 40px auto;
   position: relative;
   box-shadow: ${({ theme }) => `0px 0px 5px ${theme.colors.gray}`};
