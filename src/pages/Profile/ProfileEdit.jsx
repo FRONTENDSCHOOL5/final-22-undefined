46554f7c210da0f2b156782eab053e63959e190b
Jiyun Park <jiyun991127@gmail.com>
2023-06-15 23:49:23 +0900

    feat: 프로필 수정 페이지에서 필요한 상태 선언 및 props 전달 (#24)

--- src/pages/Profile/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SaveHeader from '../../components/common/Header/SaveHeader';
 import * as S from './ProfileEdit.style';
+import ProfileForm from '../../components/ProfileForm/ProfileForm';
 
 const ProfileEdit = () => {
+  const [formData, setFormData] = useState({
+    accountname: '',
+    username: '',
+    intro: '',
+  });
+  const [img, setImg] = useState('');
+  const [error, setError] = useState({
+    accountname: '',
+    username: '',
+    intro: '',
+  });
   return (
     <S.Main>
       <S.LayoutWrapper>
         <S.Title className='a11y-hidden'>프로필 수정 페이지</S.Title>
         <SaveHeader name='저장' mode='default' />
-        <S.Form></S.Form>
+        <S.Form>
+          <ProfileForm
+            formData={formData}
+            setFormData={setFormData}
+            img={img}
+            setImg={setImg}
+            error={error}
+            setError={setError}
+          />
+        </S.Form>
       </S.LayoutWrapper>
     </S.Main>
   );
