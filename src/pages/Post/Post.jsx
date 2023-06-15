9c754a17ac155066e1682a0e55c96e8945a7f576
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-15 16:59:46 +0900

    refactor: 게시글 이미지 업로드 확장자명 예외처리 추가(#21)

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import uploadIcon from '../../assets/icon/icon-upload.png';
 
+const ALLOWED_EXTENSIONS = ['.jpg', '.gif', '.png', '.jpeg', '.bmp', '.tif', '.heic'];
+const Label = styled.label`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: ${({ theme }) => `${theme.colors.primary} url(${uploadIcon}) no-repeat center`};
+`;
 const Post = () => {
   const [imgName, setImgName] = useState('');
 
-  const handleImgInput = (e) => {
+  const handleImgInput = async (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      return;
+    }
+
+    const file = e.target.files[0];
+    console.log(file.name.split('.'));
+    const fileExtenstion = file.name.split('.');
+    console.log(fileExtenstion);
+    if (!ALLOWED_EXTENSIONS.includes(`.${fileExtenstion}`)) return;
+
     const formData = new FormData();
-    const imgFile = e.target.files[0];
-    console.log(e.target.files);
-    console.log(imgFile);
-    formData.append('image', imgFile);
+    formData.append('image', file);
 
     try {
-      const response = fetch('https://api.mandarin.weniv.co.kr/', {
+      const response = await fetch('https://api.mandarin.weniv.co.kr/image/uploadfile', {
         method: 'POST',
         body: formData,
       });
-      const data = response.json();
+      const data = await response.json();
       console.log(data);
     } catch (error) {
       console.log(error.message);
@@ -24,10 +43,8 @@ const Post = () => {
 
   return (
     <>
-      <label htmlFor='postImg'>
-        <img src='https://api.mandarin.weniv.co.kr/*' alt='게시글 업로드 이미지' />
-      </label>
-      <input type='file' id='profileImg' onChange={handleImgInput} accept='image/*' />
+      <Label htmlFor='postImg'></Label>
+      <input className='a11y-hidden' type='file' id='postImg' onChange={handleImgInput} accept='image/*' />
     </>
   );
 };
