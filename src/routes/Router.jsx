e84e0bfffba9dacdc97fff3bfda30eccd967e104
goyomi <ghdl1004797@gmail.com>
2023-06-16 22:08:19 +0900

    style: search 페이지 라우터로 페이지 경로 지정하기(#32)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Loading from '../pages/Loading/Loading';
 import Home from '../pages/Home/Home';
+import Search from '../pages/Search/Search';
 import Join from '../pages/Join/Join';
 import ProfileSetting from '../pages/Join/ProfileSetting';
 import Login from '../pages/Login/Login';
@@ -12,6 +13,7 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<Loading />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/search' element={<Search />} />
         <Route path='/login' element={<Login />} />
         <Route path='/join' element={<Join />} />
         <Route path='/join/profile' element={<ProfileSetting />} />
