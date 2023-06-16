4185ecf3e31e0fef5c9d564b4fa4d2cb8c30845f
hyeonbin jeong <duafuuu@gmail.com>
2023-06-17 02:13:07 +0900

    add: 프로필 피드 페이지 공통 컴포넌트 FeedHeader 파일 추가 #27

--- src/pages/Profile/FeedHeader.jsx
+++ src/pages/Profile/FeedHeader.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from './FeedHeader.style';
 import backButtonImg from './../../assets/icon/icon-arrow-left.png';
 import ModalButtonImg from './../../assets/icon/icon-more-vertical.png';
-import { useNavigate } from 'react-router-dom';
-import PostModal from '../../components/common/Modal/PostModal';
+import Modal from './../../components/common/Modal/Modal';
 
 const FeedHeader = () => {
-  const navigate = useNavigate(); // useNavigate 함수로 호출
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
 
   const backHandle = (e) => {
     e.preventDefault();
     navigate(-1); // 이전 페이지 이동
   };
 
-  const ModalHandleOpen = () => {
+  const openModal = () => {
     setIsModalOpen(true);
   };
 
@@ -22,21 +22,19 @@ const FeedHeader = () => {
     setIsModalOpen(false);
   };
 
-  const modalOptions = ['설정 및 개인정보', '로그아웃'];
+  const feedModalOptions = ['설정 및 개인정보', '로그아웃'];
 
   return (
     <S.Header>
       <S.ButtonIcon onClick={backHandle}>
         <img src={backButtonImg} alt='뒤로가기 버튼' />
       </S.ButtonIcon>
-      <S.ButtonIcon onClick={ModalHandleOpen}>
+      <S.ButtonIcon onClick={openModal}>
         <img src={ModalButtonImg} alt='숨겨진 모달창 나타내기' />
       </S.ButtonIcon>
       {isModalOpen && (
         <>
-          <S.BgDark>
-            <PostModal options={modalOptions} onClose={closeModal} />
-          </S.BgDark>
+          <Modal options={feedModalOptions} onClose={closeModal} />
         </>
       )}
     </S.Header>
--- src/pages/Profile/FeedHeader.style.jsx
+++ src/pages/Profile/FeedHeader.style.jsx
@@ -14,13 +14,3 @@ export const ButtonIcon = styled.button`
   height: 22px;
   margin: 0 12px;
 `;
-
-export const BgDark = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.3);
-  z-index: 999;
-`;
