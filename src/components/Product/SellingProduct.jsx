7e44fb7a8d12188ec4afb3bbdd3c1b8b7cae013d
Jiyun Park <jiyun991127@gmail.com>
2023-06-18 03:07:27 +0900

    feat: 로딩중일 때 Skeleton 컴포넌트 추가 (#34)

--- src/components/Product/SellingProduct.jsx
+++ src/components/Product/SellingProduct.jsx
@@ -1,48 +1,30 @@
 import React from 'react';
-import Orange from '../../assets/product-img-example.png';
-import JejuOrange from '../../assets/basic-profile-img.png';
 import * as S from './SellingProduct.style';
+import ProductSkeleton from '../Skeleton/ProductSkeleton';
 
-const SellingProduct = () => {
+const SellingProduct = ({ isLoading, products }) => {
   return (
     <S.Section>
       <S.Title>판매 중인 상품</S.Title>
       <S.List>
-        <S.Item>
-          <S.ImgCover>
-            <S.Img src={Orange} alt='애월읍 노지 감귤' />
-          </S.ImgCover>
-          <S.Name className='sl-ellipsis'>애월읍 노지 감귤 애월읍 노지 감귤 애월읍 노지 감귤 애월읍 노지 감귤</S.Name>
-          <S.Price className='sl-ellipsis'>35,000원</S.Price>
-        </S.Item>
-        <S.Item>
-          <S.ImgCover>
-            <S.Img src={JejuOrange} alt='애월읍 노지 감귤' />
-          </S.ImgCover>
-          <S.Name className='sl-ellipsis'>애월읍 노지 감귤</S.Name>
-          <S.Price className='sl-ellipsis'>999,999,999,100,999,999,999원</S.Price>
-        </S.Item>
-        <S.Item>
-          <S.ImgCover>
-            <S.Img src={JejuOrange} alt='애월읍 노지 감귤' />
-          </S.ImgCover>
-          <S.Name className='sl-ellipsis'>애월읍 노지 감귤</S.Name>
-          <S.Price className='sl-ellipsis'>999,999,999,100,999,999,999원</S.Price>
-        </S.Item>
-        <S.Item>
-          <S.ImgCover>
-            <S.Img src={JejuOrange} alt='애월읍 노지 감귤' />
-          </S.ImgCover>
-          <S.Name className='sl-ellipsis'>애월읍 노지 감귤</S.Name>
-          <S.Price className='sl-ellipsis'>999,999,999,100,999,999,999원</S.Price>
-        </S.Item>
-        <S.Item>
-          <S.ImgCover>
-            <S.Img src={JejuOrange} alt='애월읍 노지 감귤' />
-          </S.ImgCover>
-          <S.Name className='sl-ellipsis'>애월읍 노지 감귤</S.Name>
-          <S.Price className='sl-ellipsis'>999,999,999,100,999,999,999원</S.Price>
-        </S.Item>
+        {isLoading ? (
+          <>
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+            <ProductSkeleton />
+          </>
+        ) : (
+          products.map((product) => (
+            <S.Item key={product.id}>
+              <S.ImgCover>
+                <S.Img src={product.itemImage} />
+              </S.ImgCover>
+              <S.Name className='s1-ellipsis'>{product.itemName}</S.Name>
+              <S.Price className='s1-ellipsis'>{`${product.price.toLocaleString()}원`}</S.Price>
+            </S.Item>
+          ))
+        )}
       </S.List>
     </S.Section>
   );
