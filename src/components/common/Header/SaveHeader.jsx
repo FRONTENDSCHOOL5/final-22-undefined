89bdffcba3d912ae2b960d735d871799b52bd800
Jiyun Park <jiyun991127@gmail.com>
2023-06-17 12:47:04 +0900

    feat: 화살표 클릭 시 뒤로 가기 기능 추가 (#25)

--- src/components/common/Header/SaveHeader.jsx
+++ src/components/common/Header/SaveHeader.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import * as S from './SaveHeader.style';
+import { useNavigate } from 'react-router-dom';
 
 const SaveHeader = ({ name, mode, ...props }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+
   return (
     <S.Header>
-      <S.BackBtn>
+      <S.BackBtn onClick={handleClick}>
         <span className='a11y-hidden'>뒤로 가기 버튼</span>
       </S.BackBtn>
       <S.StyledButton mode={mode} size='ms' {...props}>
