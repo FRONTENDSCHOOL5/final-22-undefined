2b3d4c0c6729a657cc2891443373bf2797444b44
Jiyun Park <jiyun991127@gmail.com>
2023-06-17 23:17:08 +0900

    style: styled-components 분리 (#19)

--- src/components/Post/PostAlbum.jsx
+++ src/components/Post/PostAlbum.jsx
@@ -1,66 +1,38 @@
 import React from 'react';
-import styled from 'styled-components';
 import Cat from '../../assets/upload-file.png';
 import Orange from '../../assets/post-img-example.png';
-import layers from '../../assets/icon/iccon-img-layers.svg';
-
-const Container = styled.div`
-  padding: 16px;
-`;
-
-const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 8px;
-`;
-
-const Item = styled.li`
-  height: 114px;
-  position: relative;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    width: 15px;
-    height: 15px;
-    background: url(${layers}) no-repeat center;
-  }
-`;
-
-const Img = styled.img`
-  height: 100%;
-  object-fit: cover;
-`;
+import * as S from './PostAlbum.style';
 
 const PostAlbum = () => {
   return (
-    <Container>
-      <List>
-        <Item>
-          <Img src={Cat} />
-        </Item>
-        <Item>
-          <Img src={Orange} />
-        </Item>
-        <Item>
-          <Img src={Orange} />
-        </Item>
-        <Item>
-          <Img src={Orange} />
-        </Item>
-        <Item>
-          <Img src={Orange} />
-        </Item>
-        <Item>
-          <Img src={Orange} />
-        </Item>
-        <Item>
-          <Img src={Orange} />
-        </Item>
-      </List>
-    </Container>
+    <S.Container>
+      <S.List>
+        <S.Item>
+          <S.Img src={Cat} />
+        </S.Item>
+        <S.Item>
+          <S.Img src={Orange} />
+        </S.Item>
+        <S.Item>
+          <S.Img src={Orange} />
+        </S.Item>
+        <S.Item>
+          <S.Img src={Orange} />
+        </S.Item>
+        <S.Item>
+          <S.Img src={Orange} />
+        </S.Item>
+        <S.Item>
+          <S.Img src={Orange} />
+        </S.Item>
+        <S.Item>
+          <S.Img src={Orange} />
+        </S.Item>
+        <S.Item>
+          <S.Img src={Orange} />
+        </S.Item>
+      </S.List>
+    </S.Container>
   );
 };
 
