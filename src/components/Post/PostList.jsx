46f15b86ec15728b47c965ccd756da7bc80b3328
Jiyun Park <jiyun991127@gmail.com>
2023-06-18 01:20:01 +0900

    feat: 게시글 목록 리스트형, 앨범형 조건부 렌더링 (#34)

---
+++ src/components/Post/PostList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostList = () => {
+  return <div>PostList</div>;
+};
+
+export default PostList;
--- src/components/Post/PostSection.jsx
+++ src/components/Post/PostSection.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './PostSection.style';
 import PostAlbum from './PostAlbum';
+import PostList from './PostList';
 
 const PostSection = () => {
+  const [isList, setIsList] = useState(true);
+
   return (
     <S.Section>
       <S.Title className='a11y-hidden'>포스트</S.Title>
       <S.Header>
-        <S.ListBtn>
+        <S.ListBtn onClick={() => setIsList(true)} isList={isList}>
           <span className='a11y-hidden'>리스트로 보기</span>
         </S.ListBtn>
-        <S.AlbumBtn>
-          <span className='a11y-hidden'>리스트로 보기</span>
+        <S.AlbumBtn onClick={() => setIsList(false)} isAlbum={!isList}>
+          <span className='a11y-hidden'>앨범으로 보기</span>
         </S.AlbumBtn>
       </S.Header>
-      <PostAlbum />
+      {isList ? <PostList /> : <PostAlbum />}
     </S.Section>
   );
 };
