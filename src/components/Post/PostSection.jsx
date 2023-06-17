c1d4d909a275c400361f51e272b9f3ba0963c6be
Jiyun Park <jiyun991127@gmail.com>
2023-06-18 01:07:11 +0900

    style: PostSection의 styled-components 분리 (#19)

--- src/components/Post/PostSection.jsx
+++ src/components/Post/PostSection.jsx
@@ -1,55 +1,21 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-import ListOnIcon from '../../assets/icon/icon-post-list-on.png';
-import ListOffIcon from '../../assets/icon/icon-post-list-off.png';
-import AlbumOnIcon from '../../assets/icon/icon-post-album-on.png';
-import AlbumOffIcon from '../../assets/icon/icon-post-album-off.png';
+import * as S from './PostSection.style';
 import PostAlbum from './PostAlbum';
-
-const Section = styled.section`
-  margin-top: 6px;
-  ${({ theme }) => css`
-    background-color: ${theme.colors.white};
-    border-top: 0.5px solid ${theme.colors.gray};
-  `};
-`;
-
-const Title = styled.h2``;
-
-const Header = styled.header`
-  border-bottom: ${({ theme }) => `0.5px solid ${theme.colors.gray}`};
-  padding: 5px 21px 0 0;
-  height: 44px;
-  text-align: right;
-`;
-
-const ListBtn = styled.button`
-  width: 25px;
-  height: 25px;
-  background: url(${ListOnIcon}) no-repeat center;
-  margin-right: 22px;
-`;
-
-const AlbumBtn = styled.button`
-  width: 25px;
-  height: 25px;
-  background: url(${AlbumOffIcon}) no-repeat center;
-`;
 
 const PostSection = () => {
   return (
-    <Section>
-      <Title className='a11y-hidden'>포스트</Title>
-      <Header>
-        <ListBtn>
+    <S.Section>
+      <S.Title className='a11y-hidden'>포스트</S.Title>
+      <S.Header>
+        <S.ListBtn>
           <span className='a11y-hidden'>리스트로 보기</span>
-        </ListBtn>
-        <AlbumBtn>
+        </S.ListBtn>
+        <S.AlbumBtn>
           <span className='a11y-hidden'>리스트로 보기</span>
-        </AlbumBtn>
-      </Header>
+        </S.AlbumBtn>
+      </S.Header>
       <PostAlbum />
-    </Section>
+    </S.Section>
   );
 };
 
---
+++ src/components/Post/PostSection.style.jsx
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components';
+import ListOnIcon from '../../assets/icon/icon-post-list-on.png';
+import ListOffIcon from '../../assets/icon/icon-post-list-off.png';
+import AlbumOnIcon from '../../assets/icon/icon-post-album-on.png';
+import AlbumOffIcon from '../../assets/icon/icon-post-album-off.png';
+
+export const Section = styled.section`
+  margin-top: 6px;
+  ${({ theme }) => css`
+    background-color: ${theme.colors.white};
+    border-top: 0.5px solid ${theme.colors.gray};
+  `};
+`;
+
+export const Title = styled.h2``;
+
+export const Header = styled.header`
+  border-bottom: ${({ theme }) => `0.5px solid ${theme.colors.gray}`};
+  padding: 5px 21px 0 0;
+  height: 44px;
+  text-align: right;
+`;
+
+export const ListBtn = styled.button`
+  width: 25px;
+  height: 25px;
+  background: url(${ListOnIcon}) no-repeat center;
+  margin-right: 22px;
+`;
+
+export const AlbumBtn = styled.button`
+  width: 25px;
+  height: 25px;
+  background: url(${AlbumOffIcon}) no-repeat center;
+`;
