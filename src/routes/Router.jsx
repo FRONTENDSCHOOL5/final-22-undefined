92ef184f08d32ba5e420e791c2c3d2c45a041eaf
Jiyun Park <jiyun991127@gmail.com>
2023-06-17 13:44:11 +0900

    feat: YourProfile 페이지 라우터 설정 (#34)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -7,6 +7,7 @@ import ProfileSetting from '../pages/Join/ProfileSetting';
 import Login from '../pages/Login/Login';
 import ProfileEdit from '../pages/Profile/ProfileEdit';
 import MyProfile from '../pages/Profile/MyProfile';
+import YourProfile from '../pages/Profile/YourProfile';
 
 const Router = () => {
   return (
@@ -17,8 +18,9 @@ const Router = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/join' element={<Join />} />
         <Route path='/join/profile' element={<ProfileSetting />} />
-        <Route path='/profile/edit' element={<ProfileEdit />} />
         <Route path='/profile' element={<MyProfile />} />
+        <Route path='/profile/:accountname' element={<YourProfile />} />
+        <Route path='/profile/edit' element={<ProfileEdit />} />
       </Routes>
     </BrowserRouter>
   );
