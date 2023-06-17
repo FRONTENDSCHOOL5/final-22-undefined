8202f402478187313f839405da3a4b45442095d0
goyomi <ghdl1004797@gmail.com>
2023-06-17 22:39:48 +0900

    refactor: main 파트 파일 따로 분리 후 코드 정리 (#8)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,51 +1,15 @@
 import React from 'react';
 import TopMainNav from './TopMainNav';
+import Contents from './Contents';
 import TabMenu from './TabMenu';
 
-import home from '../../assets/symbol-logo-gray.png';
-import styled from 'styled-components';
 import Wrapper from '../../components/common/Wrapper/Wrapper';
-
-const Main = styled.main`
-  text-align: center;
-`;
-
-const P = styled.p`
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 14px;
-  margin-bottom: 20px;
-  &::before {
-    content: '';
-    display: block;
-    background: url(${home}) no-repeat center/ 100px 100px;
-    width: 100px;
-    height: 100px;
-    margin: 0 auto 25px;
-  }
-`;
-
-const Btn = styled.button`
-  width: 120px;
-  height: 44px;
-  display: inline-block;
-  background: #f26e22;
-  border-radius: 44px;
-  color: #ffff;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 17.53px;
-  text-align: center;
-`;
 
 const Home = () => {
   return (
     <Wrapper>
       <TopMainNav />
-      <Main>
-        <P>유저를 검색해 팔로우 해보세요!</P>
-        <Btn>검색하기</Btn>
-      </Main>
+      <Contents />
       <TabMenu />
     </Wrapper>
   );
