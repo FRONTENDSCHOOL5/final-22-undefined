a7b01aeda8ec652887f7677c96c85db9812391bb
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-17 22:55:06 +0900

    style: post라우터 경로 태그 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,9 +5,9 @@ import Home from '../pages/Home/Home';
 import Join from '../pages/Join/Join';
 import ProfileSetting from '../pages/Join/ProfileSetting';
 import Login from '../pages/Login/Login';
-import Post from '../pages/Post/Post';
 import ProfileEdit from '../pages/Profile/ProfileEdit';
 import MyProfile from '../pages/Profile/MyProfile';
+import PostUpload from '../pages/PostPage/PostUpload';
 
 const Router = () => {
   return (
@@ -18,7 +18,7 @@ const Router = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/join' element={<Join />} />
         <Route path='/join/profile' element={<ProfileSetting />} />
-        <Route path='/post' element={<Post />} />
+        <Route path='/post' element={<PostUpload />} />
         <Route path='/profile/edit' element={<ProfileEdit />} />
         <Route path='/profile' element={<MyProfile />} />
       </Routes>
