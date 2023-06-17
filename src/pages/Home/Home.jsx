8dbe963af51f3cfffad9ff187e38fa3dc0dd53ad
goyomi <ghdl1004797@gmail.com>
2023-06-17 15:05:46 +0900

    design: home 화면 전체 레이아웃 조정 (#8)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,40 +1,53 @@
 import React from 'react';
 import TopMainNav from './TopMainNav';
 import TabMenu from './TabMenu';
-import SearchBtn from './SearchBtn';
 
 import home from '../../assets/symbol-logo-gray.png';
 import styled from 'styled-components';
+import Wrapper from '../../components/common/Wrapper/Wrapper';
 
-const Wrap = styled.div`
+const Main = styled.main`
   text-align: center;
 `;
-const IMG = styled.img`
-  width: 100px;
-  height: 100px;
-`;
+
 const P = styled.p`
   font-size: 14px;
   font-weight: 400;
   line-height: 14px;
+  margin-bottom: 20px;
+  &::before {
+    content: '';
+    display: block;
+    background: url(${home}) no-repeat center/ 100px 100px;
+    width: 100px;
+    height: 100px;
+    margin: 0 auto 25px;
+  }
 `;
 
-const Section = styled.section`
-  max-width: 390px;
-  margin: 0 auto;
+const Btn = styled.button`
+  width: 120px;
+  height: 44px;
+  display: inline-block;
+  background: #f26e22;
+  border-radius: 44px;
+  color: #ffff;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17.53px;
+  text-align: center;
 `;
 
 const Home = () => {
   return (
-    <Section>
+    <Wrapper>
       <TopMainNav />
-      <Wrap>
-        <IMG src={home} alt='동그란 말풍선 머리에 귤 꼭지가 붙어있는 감귤 캐릭터' />
-        <p>유저를 검색해 팔로우 해보세요!</p>
-      </Wrap>
-      <SearchBtn />
+      <Main>
+        <P>유저를 검색해 팔로우 해보세요!</P>
+        <Btn>검색하기</Btn>
+      </Main>
       <TabMenu />
-    </Section>
+    </Wrapper>
   );
 };
 
