a63ac02e0e86edfc0a8145c809c2c9b429187691
Jiyun Park <jiyun991127@gmail.com>
2023-06-19 00:09:30 +0900

    feat: params의 값에 따라 Header의 제목 다르게 보여주기 (#49)

--- src/pages/FollowListPage/FollowList.jsx
+++ src/pages/FollowListPage/FollowList.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 import Wrapper from '../../components/common/Wrapper/Wrapper';
 import FollowListHeader from '../../components/common/Header/FollowListHeader';
+import { useParams } from 'react-router-dom';
 
 const FollowList = () => {
+  const { accountname, type } = useParams();
+  const [followList, setFollowList] = useState([]);
+
   return (
     <Wrapper>
-      <FollowListHeader type='Followers' />
+      <FollowListHeader type={type === 'follower' ? 'Followers' : 'Followings'} />
     </Wrapper>
   );
 };
