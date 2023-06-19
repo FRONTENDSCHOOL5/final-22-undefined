0e364a30bad27f531f1e0f4be9a5be009429aae5
Jiyun Park <jiyun991127@gmail.com>
2023-06-20 01:06:22 +0900

    feat: 프로필 페이지 통합에 따른 라우터 수정 (#57)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -7,18 +7,15 @@ import Join from '../pages/JoinPage/Join';
 import ProfileSetting from '../pages/JoinPage/ProfileSetting';
 import Login from '../pages/LoginPage/Login';
 import ProfileEdit from '../pages/ProfilePage/ProfileEdit';
-import MyProfile from '../pages/ProfilePage/MyProfile';
-import YourProfile from '../pages/ProfilePage/YourProfile';
 import AddProduct from '../pages/ProductPage/AddProduct';
 import FollowList from '../pages/FollowListPage/FollowList';
 import PostUpload from '../pages/PostPage/PostUpload';
-import PostContent from '../components/Post/PostContent';
+import ProfilePage from '../pages/ProfilePage/ProfilePage';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/post/:accountname/userpost' element={<PostContent />} />
         <Route path='/' element={<Loading />} />
         <Route path='/home' element={<Home />} />
         <Route path='/search' element={<Search />} />
@@ -26,8 +23,8 @@ const Router = () => {
         <Route path='/join' element={<Join />} />
         <Route path='/join/profile' element={<ProfileSetting />} />
         <Route path='/post' element={<PostUpload />} />
-        <Route path='/profile' element={<MyProfile />} />
-        <Route path='/profile/:accountname' element={<YourProfile />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/profile/:accountname' element={<ProfilePage />} />
         <Route path='/profile/edit' element={<ProfileEdit />} />
         <Route path='/product' element={<AddProduct />} />
         <Route path='/follow/:accountname/:type' element={<FollowList />} />
