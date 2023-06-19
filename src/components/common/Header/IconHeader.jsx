9421e7116e7b92283ba2ae7bd77211524b8f3e89
hyeonbin jeong <duafuuu@gmail.com>
2023-06-20 05:08:31 +0900

    style: 모달 아이콘 사용 및 추후에 삭제될 파일

--- src/components/common/Header/IconHeader.jsx
+++ src/components/common/Header/IconHeader.jsx
@@ -23,6 +23,7 @@ const IconHeader = () => {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
       {isModalOpen && type === 'post' && <PostModal onClose={closeModal} />}
       {isModalOpen && type === 'product' && <ProductModal onClose={closeModal} />}
 =======
@@ -39,6 +40,11 @@ const IconHeader = () => {
       {isModalOpen && type === 'product' && <ProductModal onClose={closeModal} />}
       {/* product 부분 댓글로 재사용가능하지 않을까? CommentModal!! */}
 >>>>>>> e5c8868 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
+=======
+      {isModalOpen && <PostModal onClose={closeModal} postId={'64901465b2cb2056633c6795'} />}
+      {/* {isModalOpen && type === 'product' && <ProductModal onClose={closeModal} />} */}
+      {/* product 부분 댓글로 재사용가능하지 않을까? Comment!! */}
+>>>>>>> 134d3dd (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
     </>
   );
 };
