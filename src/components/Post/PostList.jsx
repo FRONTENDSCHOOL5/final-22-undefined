3fc299ea33be4677f32504f1a55149b8e5c94dec
Jiyun Park <jiyun991127@gmail.com>
2023-06-19 15:20:20 +0900

    feat: PostSection에 필요한 상태 정의 및 게시글 목록 받아오기 (#57)

--- src/components/Post/PostList.jsx
+++ src/components/Post/PostList.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const PostList = () => {
-  return <div>PostList</div>;
+const Container = styled.div`
+  padding: 16px;
+`;
+
+const NoPost = styled.p`
+  font-size: 14px;
+`;
+
+const PostList = ({ isLoading }) => {
+  return <Container>{isLoading ? <NoPost>아직 작성된 게시물이 없습니다.</NoPost> : 'postList'}</Container>;
 };
 
 export default PostList;
