05a8956f78481ab37272a7ccb73930f258145340
Jiyun Park <jiyun991127@gmail.com>
2023-06-20 03:23:37 +0900

    style: followList에 필요한 부분 마크업 (#51)

--- src/pages/FollowListPage/FollowList.jsx
+++ src/pages/FollowListPage/FollowList.jsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Wrapper from '../../components/common/Wrapper/Wrapper';
 import FollowListHeader from '../../components/common/Header/FollowListHeader';
 import { useParams } from 'react-router-dom';
+import Follows from '../../components/Follow/Follows';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  margin-top: 48px;
+  padding: 24px 16px;
+  overflow: auto;
+  max-height: calc(80vh - 48px);
+`;
 
 const FollowList = () => {
   const { accountname, type } = useParams();
   const [followList, setFollowList] = useState([]);
 
+  // useEffect(() => {
+  //   const getFollowList = async () => {
+  //     try {
+  //       const response = await fetch('https://api.mandarin.weniv.co.kr/profile/')
+  //     } catch(err) {
+  //       console.log(err.message);
+  //     }
+  //   };
+  // }, []);
+
   return (
     <Wrapper>
-      <FollowListHeader type={type === 'follower' ? 'Followers' : 'Followings'} />
+      <Main>
+        <FollowListHeader type={type === 'follower' ? 'Followers' : 'Followings'} />
+        <Follows followList={followList} />
+      </Main>
     </Wrapper>
   );
 };
