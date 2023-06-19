6b7da9eec3cc445c4083642024a2f9ee962778ac
Jiyun Park <jiyun991127@gmail.com>
2023-06-19 12:14:42 +0900

    feat: 판매중인 상품 GET 요청 (#34)

--- src/components/Product/SellingProduct.jsx
+++ src/components/Product/SellingProduct.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import * as S from './SellingProduct.style';
 import ProductSkeleton from '../Skeleton/ProductSkeleton';
+import { useParams } from 'react-router-dom';
+import { AuthContextStore } from '../../context/AuthContext';
 
-const SellingProduct = ({ isLoading, products }) => {
+const SellingProduct = () => {
+  const { accountname } = useParams();
+  const { userToken, userAccountname } = useContext(AuthContextStore);
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const userId = accountname ? accountname : JSON.parse(userAccountname);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(`https://api.mandarin.weniv.co.kr/product/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${JSON.parse(userToken)}`,
+            'Content-type': 'application/json',
+          },
+        });
+
+        const jsonData = await response.json();
+        setProducts(jsonData.product);
+
+        setIsLoading(false);
+        // setTimeout(() => {
+        //   setProducts(productList);
+        //   setIsLoading(false);
+        // }, 1000);
+      } catch (err) {
+        console.log(err.message);
+        setIsLoading(false);
+      }
+    };
+    getProducts();
+  }, [userToken, userId]);
+
   return (
     <S.Section>
       <S.Title>판매 중인 상품</S.Title>
