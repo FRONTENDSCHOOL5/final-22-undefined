5ce82d08ef6776324697466437e8e3da6aa10213
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-20 01:00:53 +0900

    feat: PostList에 공통 컴포넌트 PostItem렌더링 구현 (#52)

--- src/components/Post/PostList.jsx
+++ src/components/Post/PostList.jsx
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import PostItem from './PostItem';
+import styled from 'styled-components';
+import { AuthContextStore } from '../../context/AuthContext';
 
 const PostList = () => {
-  return <div>PostList</div>;
+  const [userProfileImg, setUserProfileImg] = useState('');
+  const [userName, setUserName] = useState('');
+  const [postList, setPostList] = useState([]);
+  const { userToken, userAccountname } = useContext(AuthContextStore);
+  const ParsedAccountName = JSON.parse(userAccountname);
+
+  // 유저 프로필 요청
+  useEffect(() => {
+    const getMyImg = async () => {
+      try {
+        const res = await fetch(`https://api.mandarin.weniv.co.kr/profile/${ParsedAccountName}`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${JSON.parse(userToken)}`,
+            'Content-type': 'application/json',
+          },
+        });
+        const data = await res.json();
+        console.log(data);
+        setUserProfileImg(data.profile.image);
+        setUserName(data.profile.username);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getMyImg();
+  }, []);
+
+  // 게시글 목록 불러오기
+  useEffect(() => {
+    const getMyContent = async () => {
+      try {
+        const res = await fetch(`https://api.mandarin.weniv.co.kr/post/${ParsedAccountName}/userpost`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${JSON.parse(userToken)}`,
+            'Content-type': 'application/json',
+          },
+        });
+        const data = await res.json();
+        console.log(data);
+        setPostList(data.post);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getMyContent();
+  }, []);
+
+  let dateString = new Date();
+  let options = { year: 'numeric', month: 'long', day: 'numeric' };
+  let today = dateString.toLocaleDateString('ko-KR', options);
+
+  return (
+    <>
+      <Ul>
+        {userToken &&
+          postList.map((post, i) => {
+            return (
+              <Li key={post.id}>
+                <PostItem
+                  userProfileImg={userProfileImg}
+                  userName={userName}
+                  acountName={ParsedAccountName}
+                  postContent={post.content}
+                  postImg={post.image}
+                  today={today}
+                />
+              </Li>
+            );
+          })}
+      </Ul>
+    </>
+  );
 };
 
 export default PostList;
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Li = styled.li``;
