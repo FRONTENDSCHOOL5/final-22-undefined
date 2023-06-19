665c96c11365a800fea36d642174e523e85d7de4
hyeonbin jeong <duafuuu@gmail.com>
2023-06-20 05:08:31 +0900

    style: 모달 아이콘 사용 및 추후에 삭제될 파일

--- src/components/common/Header/IconHeader.jsx
+++ src/components/common/Header/IconHeader.jsx
@@ -4,7 +4,7 @@ import PostModal from './../Modal/PostModal';
 import ProductModal from '../Modal/ProductModal';
 import styled from 'styled-components';
 
-const IconHeader = ({ type }) => {
+const IconHeader = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -20,8 +20,14 @@ const IconHeader = ({ type }) => {
       <ButtonIcon onClick={openModal}>
         <img src={ModalButtonImg} alt='숨겨진 모달창 나타내기' />
       </ButtonIcon>
+<<<<<<< HEAD
       {isModalOpen && type === 'post' && <PostModal onClose={closeModal} />}
       {isModalOpen && type === 'product' && <ProductModal onClose={closeModal} />}
+=======
+      {isModalOpen && <PostModal onClose={closeModal} postId={'64901465b2cb2056633c6795'} />}
+      {/* {isModalOpen && type === 'product' && <ProductModal onClose={closeModal} />} */}
+      {/* product 부분 댓글로 재사용가능하지 않을까? Comment!! */}
+>>>>>>> 134d3dd (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
     </>
   );
 };
