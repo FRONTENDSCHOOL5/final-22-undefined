111e0c4ddc79098dac79413daaa82f54e3bad46a
hyeonbin jeong <duafuuu@gmail.com>
2023-06-20 05:08:31 +0900

    style: 모달 아이콘 사용 및 추후에 삭제될 파일

--- src/components/common/Header/IconHeader.jsx
+++ src/components/common/Header/IconHeader.jsx
@@ -29,6 +29,7 @@ const IconHeader = () => {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 =======
 >>>>>>> b9ac2bc (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
@@ -36,12 +37,16 @@ const IconHeader = () => {
 >>>>>>> 5165400 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 =======
 >>>>>>> 4859fa5 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
+=======
+>>>>>>> c880721 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
       {isModalOpen && <PostModal onClose={closeModal} postId={'64901465b2cb2056633c6795'} />}
       {/* {isModalOpen && type === 'product' && <ProductModal onClose={closeModal} />} */}
       {/* product 부분 댓글로 재사용가능하지 않을까? Comment!! */}
 =======
 =======
 >>>>>>> f3c913d (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
+=======
+>>>>>>> faa93e5 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 <<<<<<< HEAD
 =======
 >>>>>>> f3c913d (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
@@ -104,10 +109,14 @@ const IconHeader = () => {
 >>>>>>> 4e4a45d (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> f3c913d (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 <<<<<<< HEAD
 >>>>>>> 79957ee (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 =======
+=======
+>>>>>>> f3c913d (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
+>>>>>>> c880721 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 =======
 =======
       {isModalOpen && type === 'post' && <PostModal onClose={closeModal} postId={'64901465b2cb2056633c6795'} />}
@@ -115,6 +124,7 @@ const IconHeader = () => {
       {/* product 부분 댓글로 재사용가능하지 않을까? CommentModal!! */}
 >>>>>>> e5c8868 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 >>>>>>> faa93e5 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
+<<<<<<< HEAD
 >>>>>>> b9ac2bc (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 =======
       {isModalOpen && <PostModal onClose={closeModal} postId={'64901465b2cb2056633c6795'} />}
@@ -127,6 +137,8 @@ const IconHeader = () => {
 =======
 >>>>>>> f3c913d (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
 >>>>>>> 4859fa5 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
+=======
+>>>>>>> c880721 (style: 모달 아이콘 사용 및 추후에 삭제될 파일)
     </>
   );
 };
