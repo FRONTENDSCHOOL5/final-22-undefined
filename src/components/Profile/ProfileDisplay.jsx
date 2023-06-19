8ba7b064c3cb7924baae5aae7d744add3b3c5396
Jiyun Park <jiyun991127@gmail.com>
2023-06-20 03:21:57 +0900

    feat: props를 전달받아 프로필 정보 렌더링 (#57)

--- src/components/Profile/ProfileDisplay.jsx
+++ src/components/Profile/ProfileDisplay.jsx
@@ -3,30 +3,37 @@ import Ellipse from '../../assets/Ellipse-1.png';
 import * as S from './ProfileDisplay.style';
 import { AuthContextStore } from '../../context/AuthContext';
 import { Link, useParams } from 'react-router-dom';
+// import PostUserProfileImg from '../Post/PostUserProfileImg';
 
-const ProfileDisplay = () => {
+const ProfileDisplay = ({ userInfo }) => {
   const { userAccountname } = useContext(AuthContextStore);
   const { accountname } = useParams();
-  const { type } = useParams();
 
   // 현재 프로필의 accountname
   const userId = accountname ? accountname : JSON.parse(userAccountname);
   // 현재 프로필에 해당하는 사람이 로그인된 유저랑 같은 사람인지 여부
   const isLoginUser = userId === JSON.parse(userAccountname);
-
   return (
     <S.Header>
       <S.Container>
-        <S.ProfileImg src={Ellipse} alt='OO의 프로필 사진' />
-        <S.UserName>크롱</S.UserName>
+        <S.ProfileImg
+          src={
+            userInfo.image === 'http://146.56.183.55:5050/Ellipse.png'
+              ? Ellipse
+              : `https://api.mandarin.weniv.co.kr/${userInfo.image}`
+          }
+          alt={`${userInfo.username}의 프로필 사진`}
+        />
+        {/* <PostUserProfileImg size='110px' userProfileImg={userInfo.image} /> */}
+        <S.UserName>{userInfo.username}</S.UserName>
         <S.AccountName>{`@ ${userId}`}</S.AccountName>
-        <S.Intro className='sl-ellipsis'>애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장입니다</S.Intro>
+        <S.Intro className='sl-ellipsis'>{userInfo.intro}</S.Intro>
         <S.FollowerLink to={`/follow/${userId}/follower`}>
-          <strong>10</strong>
+          <strong>{userInfo.followerCount}</strong>
           <S.Type>followers</S.Type>
         </S.FollowerLink>
         <S.FollowingLink to={`/follow/${userId}/following`}>
-          <strong>128</strong>
+          <strong>{userInfo.followingCount}</strong>
           <S.Type>followings</S.Type>
         </S.FollowingLink>
         {isLoginUser ? (
