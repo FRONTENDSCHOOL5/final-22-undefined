938a1a7796cc4f8706f626f3e913492271a72c4c
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-21 17:55:34 +0900

    design: 댓글 페이지 기본 마크업 (#76)

--- src/pages/PostPage/PostDetail.jsx
+++ src/pages/PostPage/PostDetail.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import FeedHeader from '../../components/common/Header/FeedHeader';
+import Comment from '../../pages/ChatPage/Comment';
+import { AuthContextStore } from '../../context/AuthContext';
+
+const defaultProfileImg = 'http://146.56.183.55:5050/Ellipse.png';
 
 const PostDetail = () => {
+  const [userProfileImg, setUserProfileImg] = useState(defaultProfileImg);
+  const { userToken } = useContext(AuthContextStore);
+
+  // 프로필 이미지 요청(댓글 페이지는 언제나 자기 프로필 사진)
+  useEffect(() => {
+    const handleUserImg = async () => {
+      try {
+        const res = await fetch('https://api.mandarin.weniv.co.kr/user/myinfo', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${JSON.parse(userToken)}`,
+          },
+        });
+        const data = await res.json();
+        setUserProfileImg(data.user.image);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    handleUserImg();
+  }, []);
   return (
     <>
       <FeedHeader />
+      <PostDetailComment />
+      <Comment defaultProfileImg={defaultProfileImg} userProfileImg={userProfileImg} />
     </>
   );
 };
