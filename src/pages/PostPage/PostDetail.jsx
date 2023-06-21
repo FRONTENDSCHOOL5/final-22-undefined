dc43a44b6e04c24f2f77c344e06cad4ae01fe23d
yongisadragon <ckfflekfxms@gmail.com>
2023-06-22 02:12:51 +0900

    design: PostDetail 마크업 (#76)

--- src/pages/PostPage/PostDetail.jsx
+++ src/pages/PostPage/PostDetail.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect, useState } from 'react';
 import FeedHeader from '../../components/common/Header/FeedHeader';
 import Comment from '../../pages/ChatPage/Comment';
 import { AuthContextStore } from '../../context/AuthContext';
+import styled from 'styled-components';
+import PostInComment from '../../components/Post/PostInComment';
+import Wrapper from '../../components/common/Wrapper/Wrapper';
 
 const defaultProfileImg = 'http://146.56.183.55:5050/Ellipse.png';
 
@@ -27,9 +30,28 @@ const PostDetail = () => {
     };
     handleUserImg();
   }, []);
+
   return (
     <>
       <FeedHeader />
+      <Main>
+        <PostWrapper>
+          <PostArticle>
+            댓글 해당 게시물
+            <h2 className='a11y-hidden'>댓글 해당 게시물</h2>
+            <PostInComment />
+          </PostArticle>
+          <PostSection>
+            <h2 className='a11y-hidden'>댓글 목록</h2>
+            <Ul>
+              <Li>
+                댓글 목록
+                {/* PostItem 혹은 PostUserProfileImg 공통 컴퍼넌트를 쓰고 싶지만 프랍스가 너무 많아서 못쓰겠어.. */}
+              </Li>
+            </Ul>
+          </PostSection>
+        </PostWrapper>
+      </Main>
       {/* <PostDetailComment /> */}
       <Comment defaultProfileImg={defaultProfileImg} userProfileImg={userProfileImg} />
     </>
@@ -37,3 +59,12 @@ const PostDetail = () => {
 };
 
 export default PostDetail;
+
+const Main = styled.main``;
+const PostWrapper = styled(Wrapper)`
+  margin-top: 48px;
+`;
+const PostArticle = styled.article``;
+const PostSection = styled.section``;
+const Ul = styled.ul``;
+const Li = styled.li``;
