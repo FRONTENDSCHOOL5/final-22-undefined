653ebd443b2d4c4d2b100f71e18810c28a764b0f
Jiyun Park <jiyun991127@gmail.com>
2023-06-21 16:10:29 +0900

    fix: userInfo가 빈 객체일 때는 SkeletonUI를 보여주도록 수정 (#86)

--- src/components/Profile/ProfileDisplay.jsx
+++ src/components/Profile/ProfileDisplay.jsx
@@ -3,9 +3,10 @@ import Ellipse from '../../assets/Ellipse-1.png';
 import * as S from './ProfileDisplay.style';
 import { AuthContextStore } from '../../context/AuthContext';
 import { Link, useParams } from 'react-router-dom';
+import ProfileSkeleton from '../Skeleton/ProfileSkeleton';
 // import PostUserProfileImg from '../Post/PostUserProfileImg';
 
-const ProfileDisplay = ({ userInfo }) => {
+const ProfileDisplay = ({ userInfo, isLoading }) => {
   const { userAccountname } = useContext(AuthContextStore);
   const { accountname } = useParams();
 
@@ -13,29 +14,36 @@ const ProfileDisplay = ({ userInfo }) => {
   const userId = accountname ? accountname : JSON.parse(userAccountname);
   // 현재 프로필에 해당하는 사람이 로그인된 유저랑 같은 사람인지 여부
   const isLoginUser = userId === JSON.parse(userAccountname);
+
   return (
     <S.Header>
       <S.Container>
-        <S.ProfileImg
-          src={
-            userInfo.image === 'http://146.56.183.55:5050/Ellipse.png'
-              ? Ellipse
-              : `https://api.mandarin.weniv.co.kr/${userInfo.image}`
-          }
-          alt={`${userInfo.username}의 프로필 사진`}
-        />
-        {/* <PostUserProfileImg size='110px' userProfileImg={userInfo.image} /> */}
-        <S.UserName>{userInfo.username}</S.UserName>
-        <S.AccountName>{`@ ${userInfo.accountname}`}</S.AccountName>
-        <S.Intro className='sl-ellipsis'>{userInfo.intro}</S.Intro>
-        <S.FollowerLink to={`/follow/${userId}/follower`}>
-          <strong>{userInfo.followerCount}</strong>
-          <S.Type>followers</S.Type>
-        </S.FollowerLink>
-        <S.FollowingLink to={`/follow/${userId}/following`}>
-          <strong>{userInfo.followingCount}</strong>
-          <S.Type>followings</S.Type>
-        </S.FollowingLink>
+        {isLoading || !userInfo.image ? (
+          <ProfileSkeleton />
+        ) : (
+          <>
+            <S.ProfileImg
+              src={
+                userInfo.image === 'http://146.56.183.55:5050/Ellipse.png'
+                  ? Ellipse
+                  : `https://api.mandarin.weniv.co.kr/${userInfo.image}`
+              }
+              alt={`${userInfo.username}의 프로필 사진`}
+            />
+            {/* <PostUserProfileImg size='110px' userProfileImg={userInfo.image} /> */}
+            <S.UserName>{userInfo.username}</S.UserName>
+            <S.AccountName>{`@ ${userInfo.accountname}`}</S.AccountName>
+            <S.Intro className='sl-ellipsis'>{userInfo.intro}</S.Intro>
+            <S.FollowerLink to={`/follow/${userId}/follower`}>
+              <strong>{userInfo.followerCount}</strong>
+              <S.Type>followers</S.Type>
+            </S.FollowerLink>
+            <S.FollowingLink to={`/follow/${userId}/following`}>
+              <strong>{userInfo.followingCount}</strong>
+              <S.Type>followings</S.Type>
+            </S.FollowingLink>
+          </>
+        )}
         {isLoginUser ? (
           <S.BtnContainer>
             <Link to='/profile/edit'>
