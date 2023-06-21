73f93126974579443db75b0f2e200fe9b2262204
Jiyun Park <jiyun991127@gmail.com>
2023-06-21 13:38:13 +0900

    feat: followList 서버에서 불러와 상태 업데이트 (#51)

--- src/pages/FollowListPage/FollowList.jsx
+++ src/pages/FollowListPage/FollowList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Wrapper from '../../components/common/Wrapper/Wrapper';
 import FollowListHeader from '../../components/common/Header/FollowListHeader';
 import { useParams } from 'react-router-dom';
 import Follows from '../../components/Follow/Follows';
 import styled from 'styled-components';
+import { AuthContextStore } from '../../context/AuthContext';
 
 const Main = styled.main`
   margin-top: 48px;
@@ -13,17 +14,24 @@ const Main = styled.main`
 
 const FollowList = () => {
   const { accountname, type } = useParams();
+  const { userToken } = useContext(AuthContextStore);
   const [followList, setFollowList] = useState([]);
 
-  // useEffect(() => {
-  //   const getFollowList = async () => {
-  //     try {
-  //       const response = await fetch('https://api.mandarin.weniv.co.kr/profile/')
-  //     } catch(err) {
-  //       console.log(err.message);
-  //     }
-  //   };
-  // }, []);
+  useEffect(() => {
+    const getFollowList = async () => {
+      try {
+        const response = await fetch(`https://api.mandarin.weniv.co.kr/profile/${accountname}/${type}`, {
+          headers: { Authorization: `Bearer ${JSON.parse(userToken)}`, 'Content-type': 'application/json' },
+        });
+
+        const data = await response.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    getFollowList();
+  }, []);
 
   return (
     <Main>
