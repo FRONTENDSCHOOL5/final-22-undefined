ab2135d70068355b893911938aff86c354ae5319
hyeonbin jeong <duafuuu@gmail.com>
2023-06-23 14:55:46 +0900

    feat: 상품 수정 기능 추가

--- src/pages/ProductPage/EditProduct.jsx
+++ src/pages/ProductPage/EditProduct.jsx
@@ -1,17 +1,121 @@
-import React from 'react';
-import Wrapper from '../../components/common/Wrapper/Wrapper';
+import React, { useEffect, useState, useContext } from 'react';
 import styled from 'styled-components';
+import Wrapper from '../../components/common/Wrapper/Wrapper';
 import SaveHeader from '../../components/common/Header/SaveHeader';
+import { AuthContextStore } from '../../context/AuthContext';
+import ProductForm from '../../components/Product/ProductForm';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const Main = styled.main``;
 
 const LayoutWrapper = styled(Wrapper)`
-  padding: 34px;
+  padding: 30px 34px;
+`;
+
+const Form = styled.form`
+  padding-top: 78px;
 `;
 
 const EditProduct = () => {
+  const { userToken } = useContext(AuthContextStore);
+  const [formData, setFormData] = useState({
+    itemName: '',
+    price: '',
+    link: '',
+  });
+  const [img, setImg] = useState('');
+  const [error, setError] = useState({
+    itemName: '',
+    price: '',
+    link: '',
+  });
+
+  const navigate = useNavigate();
+  const { product_id } = useParams();
+
+  let isActivated = false;
+  if (
+    formData.itemName.trim() !== '' &&
+    formData.price.trim() !== '' &&
+    formData.link.trim() !== '' &&
+    (error.itemName === '' || error.itemName === 'noError') &&
+    (error.price === '' || error.price === 'noError') &&
+    (error.link === '' || error.link === 'noError')
+  ) {
+    isActivated = true;
+  }
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const response = await fetch(`https://api.mandarin.weniv.co.kr/product/${product_id}`, {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
+
+        const data = await response.json();
+        const { itemName, price, link, itemImage } = data.product;
+
+        setFormData({
+          itemName,
+          price: price.toString(),
+          link,
+        });
+        setImg(itemImage);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+
+    getProduct();
+  }, [product_id, userToken]);
+
+  const addClick = async () => {
+    try {
+      const response = await fetch(`https://api.mandarin.weniv.co.kr/product/${product_id}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          product: {
+            itemName: formData.itemName,
+            price: parseInt(formData.price.replace(/[^0-9]/g, '')),
+            link: formData.link,
+            itemImage: img,
+          },
+        }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        navigate('/profile');
+      } else {
+        console.log(data.message);
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
-    <LayoutWrapper>
-      <SaveHeader name='저장'></SaveHeader>
-    </LayoutWrapper>
+    <Main>
+      <LayoutWrapper>
+        <SaveHeader name='저장' mode={isActivated ? 'default' : 'disabled'} onClick={addClick} />
+        <Form onSubmit={(e) => e.preventDefault()}>
+          <ProductForm
+            formData={formData}
+            setFormData={setFormData}
+            img={img}
+            setImg={setImg}
+            error={error}
+            setError={setError}
+          />
+        </Form>
+      </LayoutWrapper>
+    </Main>
   );
 };
 
