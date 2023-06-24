5a2e7dd527d6a8c2ba9da3366a7ebc7a6728db2c
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-24 21:03:56 +0900

    chore: 불필요 주석 제거

--- src/components/Post/PostCommentItem.jsx
+++ src/components/Post/PostCommentItem.jsx
@@ -7,7 +7,6 @@ import PostUserProfileImg from './PostUserProfileImg';
 const PostCommentItem = ({ comments }) => {
   //작성 경과 시간 함수
   const formatRelativeDate = (date) => {
-    console.log(date);
     const TEN_SECOND = 10 * 1000;
     const A_MINUTE = 60 * 1000;
     const A_HOUR = 60 * A_MINUTE;
@@ -15,8 +14,9 @@ const PostCommentItem = ({ comments }) => {
     const A_WEEK = 7 * A_DAY;
 
     const time = new Date(date);
-    console.log(time);
     const diff = new Date() - time;
+    // console.log(date);
+    // console.log(time);
     if (diff < TEN_SECOND) return `방금 전`;
     if (diff < A_MINUTE) return `${Math.floor(diff / 1000)}초 전`;
     if (diff < A_HOUR) return `${Math.floor(diff / 1000 / 60)}분 전`;
