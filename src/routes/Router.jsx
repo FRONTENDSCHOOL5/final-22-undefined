f12f467a299a0b8c80829e666d93851b5796dc5a
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-25 04:00:24 +0900

    style: /post/upload 게시물 업로드 경로 변경 및 edit라우터 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -37,6 +37,7 @@ const Router = () => {
           <Route path='/chat' element={<ChatLists />} />
           <Route path='/chat/room' element={<ChatRoom />} />
           <Route path='/post' element={<PostUpload />} />
+          <Route path='/post/edit' element={<PostUpload />} />
           <Route path='/postdetail/:post_id' element={<PostDetail />} />
           <Route path='/profile' element={<ProfilePage />} />
           <Route path='/profile/:accountname' element={<ProfilePage />} />
