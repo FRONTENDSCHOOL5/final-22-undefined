ac198a0c07aee8ec02c4af0a4e5ce91ba5afb916
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-24 17:35:11 +0900

    fix: 다른 유저 프로필에서 댓글의 프로필 이미지가 모두 자신의 이미지로 뜨는 문제 해결 (#108)

--- src/pages/PostPage/PostDetail.jsx
+++ src/pages/PostPage/PostDetail.jsx
@@ -12,15 +12,15 @@ import ModalButtonImg from '../../assets/icon/icon-more-vertical.png';
 const PostDetail = () => {
   const [userInfo, setUserInfo] = useState({});
   const { post_id } = useParams();
-  const [userProfileImg, setUserProfileImg] = useState('');
+  const [myProfileImg, setMyProfileImg] = useState('');
   const { userToken } = useContext(AuthContextStore);
   const [comments, setComments] = useState([]);
   const [isCommentUpdated, setIsCommentUpdated] = useState(false);
 
-  //userInfo를 위한 요청
   useEffect(() => {
     const getUserInfo = async () => {
       try {
+        // 게시물 정보 요청
         const response = await fetch(`https://api.mandarin.weniv.co.kr/post/${post_id}`, {
           headers: {
             Authorization: `Bearer ${userToken}`,
@@ -30,7 +30,7 @@ const PostDetail = () => {
         const data = await response.json();
         setUserInfo(data.post);
 
-        //댓글 리스트 불러오기
+        //댓글 리스트 정보 요청
         const res = await fetch(`https://api.mandarin.weniv.co.kr/post/${post_id}/comments/?limit=10&skip=0`, {
           headers: {
             Authorization: `Bearer ${userToken}`,
@@ -39,7 +39,7 @@ const PostDetail = () => {
         });
         const result = await res.json();
         setComments(result.comments);
-        setIsCommentUpdated(false); //comment에서 게시 클릭시 true로 바뀐 이후 본래 false 상태로 전환 해줘야 이후 상태를 재업데이트 할 수 있음.
+        setIsCommentUpdated(false); //comment에서 댓글을 올리면 true로 바뀌도로 설정돼있는데, 그럼 이 댓글 리스트 정보 요청이 발생하고, list가 업데이트됨. 그 이후 본래 false 상태로 전환 해줘야 이후 게시를해도 상태를 재업데이트 할 수 있음.
       } catch (err) {
         console.log(err.message);
       }
@@ -58,7 +58,7 @@ const PostDetail = () => {
           },
         });
         const data = await res.json();
-        setUserProfileImg(data.user.image);
+        setMyProfileImg(data.user.image);
       } catch (error) {
         console.log(error.message);
       }
@@ -94,7 +94,7 @@ const PostDetail = () => {
                     <Li key={item.id}>
                       <UserAuth>
                         <ProfileLink to={`/profile/${item.author.accountname}`}>
-                          <PostUserProfileImg size={'36px'} userProfileImg={userProfileImg} />
+                          <PostUserProfileImg size={'36px'} userProfileImg={item.author.image} />
                         </ProfileLink>
                         <NameLink to={`/profile/${item.author.accountname}`}>
                           <Username>{item.author.username}</Username>
@@ -115,7 +115,7 @@ const PostDetail = () => {
       <Comment
         setIsCommentUpdated={setIsCommentUpdated}
         atChatroom={false}
-        userProfileImg={userProfileImg}
+        userProfileImg={myProfileImg}
         postId={post_id}
       />
     </>
