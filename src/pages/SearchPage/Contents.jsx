4a2f2e1d7eb6f6e5300fecbc6e0737a45712aa58
goyomi <ghdl1004797@gmail.com>
2023-06-24 23:15:30 +0900

    feat: 검색 시 유저 프로필 사진 보이는 코드 추가 (#94)

--- src/pages/SearchPage/Contents.jsx
+++ src/pages/SearchPage/Contents.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PostUserProfileImg from '../../components/Post/PostUserProfileImg';
 
 import styled from 'styled-components';
 
@@ -19,6 +20,17 @@ const UserList = styled.ul`
     width: 100%;
     display: flex;
   }
+  .userName {
+    margin: 5px 0 6px;
+    line-height: 18px;
+    font-size: 14px;
+    font-weight: 500;
+  }
+  .accountName {
+    color: #767676;
+    font-size: 12px;
+    line-height: 15px;
+  }
 `;
 
 const Contents = ({ userList }) => {
@@ -28,10 +40,10 @@ const Contents = ({ userList }) => {
         <UserList key={item.id}>
           <li>
             <a>
-              <img src='' alt='' />
+              <PostUserProfileImg userProfileImg={item.image} />
               <div>
-                <p>{item.username}</p>
-                <p>{item.accountname}</p>
+                <p className='userName'>{item.username}</p>
+                <p className='accountName'>{item.accountname}</p>
               </div>
             </a>
           </li>
