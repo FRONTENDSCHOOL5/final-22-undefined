bbd102e8e01eb1140252756848a16ed47e3e39a9
goyomi <ghdl1004797@gmail.com>
2023-06-25 15:53:17 +0900

    design: splash main img 배치 (#110)

--- src/pages/SplashPage/Splash.jsx
+++ src/pages/SplashPage/Splash.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import mainIcon from '../../assets/free-icon-bff-4841304 1.png';
+
 const Splash = () => {
-  return <div>Splash</div>;
+  return (
+    <>
+      <img src={mainIcon} alt='' />
+    </>
+  );
 };
 
 export default Splash;
