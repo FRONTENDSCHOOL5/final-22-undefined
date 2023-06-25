302cbb29882835ff55890bd452e87685c7177a1f
hyeonbin jeong <duafuuu@gmail.com>
2023-06-25 19:20:15 +0900

    feat: chatModal 기능 구현 #122

--- src/components/common/Modal/ChatModal.jsx
+++ src/components/common/Modal/ChatModal.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import * as S from './Modal.style';
+import { useNavigate } from 'react-router';
 
-const ChatModal = () => {
-  return <div>ChatModal</div>;
+const ChatModal = ({ onClose }) => {
+  const modalRef = useRef();
+  const navigate = useNavigate();
+  const optionClick = (option) => {
+    if (option === '채팅방 나가기') {
+      onClose();
+      navigate('/chat');
+    }
+  };
+
+  const clickOutside = (e) => {
+    if (modalRef.current && modalRef.current === e.target) {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      <S.ModalBg ref={modalRef} onClick={clickOutside}>
+        <S.Ul>
+          <S.Li>
+            <button onClick={() => optionClick('채팅방 나가기')}>채팅방 나가기</button>
+          </S.Li>
+        </S.Ul>
+      </S.ModalBg>
+    </>
+  );
 };
 
 export default ChatModal;
