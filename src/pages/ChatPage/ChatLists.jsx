bb9e1dbafeeb2476bc7cc71d7c32def8e26234e4
goyomi <ghdl1004797@gmail.com>
2023-06-25 21:49:33 +0900

    style: MainLayout 적용, 페이지 TabMenu 아이콘 색상변경 코드 추가

--- src/pages/ChatPage/ChatLists.jsx
+++ src/pages/ChatPage/ChatLists.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TabMenu from '../../components/common/TabMenu/TabMenu';
 
 import ChatListsContents from './ChatListsContents';
 import FeedHeader from '../../components/common/Header/FeedHeader';
 
+import { useParams } from 'react-router-dom';
+import { AuthContextStore } from '../../context/AuthContext';
+
 const ChatList = () => {
+  const { accountname } = useParams();
+  const { userToken, userAccountname } = useContext(AuthContextStore);
+  // 현재 프로필의 accountname
+  const userId = accountname ? accountname : userAccountname;
+  // 현재 프로필에 해당하는 사람이 로그인된 유저랑 같은 사람인지 여부
+  const isLoginUser = userId === userAccountname;
+
   return (
     <>
       <FeedHeader />
       <ChatListsContents />
-      <TabMenu />
+      <TabMenu active={isLoginUser ? '1' : '0'} />
     </>
   );
 };
