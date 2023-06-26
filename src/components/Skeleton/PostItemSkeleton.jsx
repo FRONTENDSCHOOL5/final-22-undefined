178a8d76f99576d6072c9e48b436a07dcf6e7958
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-06-26 15:03:37 +0900

    feat:  게시글 아이템 스켈레톤 적용 (#92)

--- src/components/Skeleton/PostItemSkeleton.jsx
+++ src/components/Skeleton/PostItemSkeleton.jsx
@@ -18,6 +18,7 @@ const PostItemSkeleton = () => {
         <UserPostImg />
         <LikeAndComment>
           <LikeBtn />
+          <LikeBtn />
         </LikeAndComment>
         <TodayDate />
       </UserContentSect>
@@ -29,6 +30,8 @@ export default PostItemSkeleton;
 
 const PostArticle = styled.article`
   position: relative;
+  padding-bottom: 20px;
+  margin-bottom: 50px;
 `;
 
 const UserInfoSect = styled.div`
@@ -36,71 +39,70 @@ const UserInfoSect = styled.div`
   align-items: center;
   margin-bottom: 12px;
   border-radius: 8px;
-
-  ${loadingAnimation}
 `;
 
 const PostUserProfileImg = styled.div`
-  border-radius: 8px;
+  width: 42px;
+  height: 42px;
+  margin-right: 12px;
+  border-radius: 20px;
+  ${loadingAnimation}
 `;
 const UserNameInfo = styled.div`
-  flex-grow: 1;
   border-radius: 8px;
-
-  ${loadingAnimation}
 `;
 
 const InfoName = styled.div`
-  border-radius: 8px;
-
+  width: 140px;
+  height: 15px;
+  border-radius: 12px;
   margin-bottom: 6px;
   ${loadingAnimation}
 `;
-const InfoAccount = styled.div`
-  font-size: 12px;
-  border-radius: 8px;
 
+const InfoAccount = styled.div`
+  width: 100px;
+  height: 10px;
+  border-radius: 12px;
   ${loadingAnimation}
 `;
 
 const UserContentSect = styled.div`
   padding-left: 54px;
-  border-radius: 8px;
-
-  ${loadingAnimation}
 `;
 
 const UserPostText = styled.div`
   margin-bottom: 16px;
-  word-break: break-all;
-  line-height: 20px;
+  border-radius: 16px;
+  width: 140px;
+  height: 15px;
   ${loadingAnimation}
 `;
 
 const UserPostImg = styled.div`
   width: 100%;
-  height: 100%;
+  height: 200px;
   border-radius: 10px;
   margin-bottom: 16px;
-  border: 1px solid ${({ theme }) => theme.colors.gray};
   overflow: hidden;
   object-fit: cover;
   ${loadingAnimation}
 `;
 
 const LikeAndComment = styled.div`
+  height: 20px;
   display: flex;
   align-items: center;
   gap: 18px;
   margin-bottom: 16px;
-  ${loadingAnimation}
 `;
 
 const LikeBtn = styled.div`
+  border-radius: 12px;
+
   &::before {
     content: '';
     width: 20px;
-    height: 20px;
     margin-right: 6px;
     display: inline-block;
     vertical-align: bottom;
@@ -108,7 +110,9 @@ const LikeBtn = styled.div`
   ${loadingAnimation}
 `;
 
-const TodayDate = styled.p`
-  font-size: 10px;
+const TodayDate = styled.div`
+  width: 100px;
+  height: 10px;
+  border-radius: 8px;
   ${loadingAnimation}
 `;
