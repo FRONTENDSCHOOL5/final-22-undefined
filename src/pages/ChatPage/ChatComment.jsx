355d0c6aef64c0043ed14ecd37d78e2fa652723b
JiyunPark1301 <jiyun991127@gmail.com>
2023-06-27 21:49:25 +0900

    fix: 채팅방일 때는 작성된 댓글이 사라지고 api 통신 막기

--- src/pages/ChatPage/ChatComment.jsx
+++ src/pages/ChatPage/ChatComment.jsx
@@ -1,12 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, { useContext, useState, forwardRef } from 'react';
 =======
 import React, { useContext, useState } from 'react';
 >>>>>>> 92424a5 (move: style 파일 전체 분리)
+=======
+import React, { useContext, useState, forwardRef } from 'react';
+>>>>>>> 211b664 (fix: 채팅방일 때는 작성된 댓글이 사라지고 api 통신 막기)
 import * as S from './ChatComment.style';
 import PostUserProfileImg from '../../components/Post/PostUserProfileImg';
 import { AuthContextStore } from '../../context/AuthContext';
 import { addComment } from '../../api/comment';
+<<<<<<< HEAD
 <<<<<<< HEAD
 import { useLocation } from 'react-router-dom';
 
@@ -21,17 +26,25 @@ const ChatComment = forwardRef(({ setCommentList, atChatroom, userProfileImg, po
     try {
       const data = await addComment(postId, text, userToken);
 =======
+=======
+import { useLocation } from 'react-router-dom';
+>>>>>>> 211b664 (fix: 채팅방일 때는 작성된 댓글이 사라지고 api 통신 막기)
 
-const ChatComment = ({ setCommentList, atChatroom, userProfileImg, postId, setCommentCnt }) => {
+const ChatComment = forwardRef(({ setCommentList, atChatroom, userProfileImg, postId, setCommentCnt }, ref) => {
   const [text, setText] = useState('');
   const { userToken } = useContext(AuthContextStore);
+  const location = useLocation();
 
   const submitComment = async (e) => {
     e.preventDefault();
+    if (location.pathname === '/chat/room') return setText('');
     try {
       const data = await addComment(postId, text, userToken);
+<<<<<<< HEAD
       // console.log(data);
 >>>>>>> 92424a5 (move: style 파일 전체 분리)
+=======
+>>>>>>> 211b664 (fix: 채팅방일 때는 작성된 댓글이 사라지고 api 통신 막기)
       setCommentCnt((prev) => prev + 1);
       setCommentList((prev) => [data.comment, ...prev]);
       setText('');
@@ -69,9 +82,13 @@ const ChatComment = ({ setCommentList, atChatroom, userProfileImg, postId, setCo
           placeholder={atChatroom ? '메시지 입력하기...' : '댓글 입력하기...'}
           onChange={handleOnChange}
 <<<<<<< HEAD
+<<<<<<< HEAD
           ref={ref}
 =======
 >>>>>>> 92424a5 (move: style 파일 전체 분리)
+=======
+          ref={ref}
+>>>>>>> 211b664 (fix: 채팅방일 때는 작성된 댓글이 사라지고 api 통신 막기)
         />
         <S.PostBtn isActivated={isActivated} type='submit'>
           {atChatroom ? '전송' : '게시'}
@@ -80,9 +97,13 @@ const ChatComment = ({ setCommentList, atChatroom, userProfileImg, postId, setCo
     </S.Footer>
   );
 <<<<<<< HEAD
+<<<<<<< HEAD
 });
 =======
 };
 >>>>>>> 92424a5 (move: style 파일 전체 분리)
+=======
+});
+>>>>>>> 211b664 (fix: 채팅방일 때는 작성된 댓글이 사라지고 api 통신 막기)
 
 export default ChatComment;
