2a71ba15a0b2a9a9d193749228757482f3994c14
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-07-17 14:31:26 +0900

    test: 맵 마커 추가 테스트

--- src/pages/MapPage/Kakao.jsx
+++ src/pages/MapPage/Kakao.jsx
@@ -5,11 +5,16 @@ const Kakao = () => {
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-      level: 3,
+      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
     };
-    const map = new kakao.maps.Map(container, options);
+    const map = new kakao.maps.Map(container, options); // 지도를 생성
     console.log('loading');
+    const markerPosition = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488);
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    marker.setMap(map);
   }, []);
   return (
     <>
