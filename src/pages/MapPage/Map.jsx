0991e85e0f7465070490bacb8abd8407cf5852f8
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-07-18 00:00:03 +0900

    test

--- src/pages/MapPage/Map.jsx
+++ src/pages/MapPage/Map.jsx
@@ -1,12 +1,14 @@
 import TabMenu from '../../components/common/TabMenu/TabMenu';
 import FeedHeader from '../../components/common/Header/FeedHeader';
 import Kakao from './Kakao';
+import MapSdk from './MapSdk';
 
 const Map = () => {
   return (
     <div>
       <FeedHeader />
-      <Kakao />
+      {/* <Kakao /> */}
+      <MapSdk />
       <TabMenu active={'3'} />
     </div>
   );
