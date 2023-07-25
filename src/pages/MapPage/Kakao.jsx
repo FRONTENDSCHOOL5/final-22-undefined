1ac089eef2248784d1dbbd208f4bf4608bf7d222
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-07-25 12:19:28 +0900

    feat: 맵 화면에 가져오기 (#177)

--- src/pages/MapPage/Kakao.jsx
+++ src/pages/MapPage/Kakao.jsx
@@ -1,52 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 const { kakao } = window;
 
 const Kakao = () => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
-  const BASE_URL = 'https://api.odcloud.kr/api';
-  const serviceKey = 'HQpb9jbWMbzoTG5dwYCTcwnrcsNAY3mSP%2BoPWsCttfLDD7Y4IGiU964GMWSsWYrPxkNI%2BoQlSDUVnGNOf0mT5w%3D%3D';
+
+  // 기본 위치 상태
+  const [state, setState] = useState({
+    center: {
+      lat: 33.450701,
+      lng: 126.570667,
+    },
+    errMsg: null,
+    isLoading: true,
+  });
+
+  // geolocation
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const data2 = await fetch(
-          `${BASE_URL}/15111389/v1/uddi:41944402-8249-4e45-9e9d-a52d0a7db1cc?page=1&perPage=200&serviceKey=${serviceKey}`,
-          {
-            method: 'GET',
-          },
-        );
-        const result = await data2.json();
-        console.log(result);
-      } catch (error) {
-        console.error('실패:', error);
-      }
-    };
-    getData();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setState((prev) => ({
+            ...prev,
+            center: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            },
+            isLoading: false,
+          }));
+        },
+        (err) => {
+          setState((prev) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false,
+          }));
+        },
+      );
+    } else {
+      setState((prev) => ({
+        ...prev,
+        errMsg: 'geolocation을 사용할수 없어요..',
+        isLoading: false,
+      }));
+    }
   }, []);
 
+  // resizer
   useEffect(() => {
     const resizeListener = () => {
       setInnerWidth(window.innerWidth);
       setInnerHeight(window.innerHeight);
     };
-    window.addEventListener('resize', resizeListener); //화면 사이즈 변경을 감지한다!!!
-
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488), // 지도의 중심좌표
-      level: 3, // 지도의 확대 레벨
-    };
-    const map = new kakao.maps.Map(container, options); // 지도를 생성
-    console.log('loading');
-    const markerPosition = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488);
-    const marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(map);
+    window.addEventListener('resize', resizeListener);
   }, []);
+
   return (
     <>
-      <div id='map' style={{ width: innerWidth, height: innerHeight - 60, scrollSnapMarginTop: '48px' }}></div>
+      <Map center={state.center} style={{ width: innerWidth, height: innerHeight - 60 }} level={3}>
+        {!state.isLoading && <MapMarker position={state.center}></MapMarker>}
+      </Map>
     </>
   );
 };
--- src/pages/MapPage/Map.jsx
+++ src/pages/MapPage/Map.jsx
@@ -1,14 +1,12 @@
 import TabMenu from '../../components/common/TabMenu/TabMenu';
 import FeedHeader from '../../components/common/Header/FeedHeader';
 import Kakao from './Kakao';
-import MapSdk from './MapSdk';
 
 const Map = () => {
   return (
     <div>
       <FeedHeader />
-      {/* <Kakao /> */}
-      <MapSdk />
+      <Kakao />
       <TabMenu active={'3'} />
     </div>
   );
