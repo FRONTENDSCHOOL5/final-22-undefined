8dcb5dc488e9b563976e1a346d4b4e58f4633a45
hyeonbin jeong <duafuuu@gmail.com>
2023-08-09 22:10:28 +0900

    style: 확인 메시지 창 하나로 통합 및 스타일 컴포넌트 병합

--- src/components/common/Modal/AlertModal.jsx
+++ src/components/common/Modal/AlertModal.jsx
@@ -1,23 +1,65 @@
 import React from 'react';
-import * as S from './AlertModal.style';
+import styled from 'styled-components';
 
-const AlertModal = ({ message, onClose, buttons, buttonFontColor, buttonBorder }) => {
+const AlertModal = ({ message, onClose, buttons }) => {
   return (
-    <S.AlertSection>
-      <S.Strong>{message}</S.Strong>
-      <S.Ul>
+    <AlertSection>
+      <Strong>{message}</Strong>
+      <Ul>
         {buttons.map((button, index) => {
           return (
-            <S.Li key={index}>
-              <S.Button onClick={() => onClose(button)} color={buttonFontColor[index]} style={buttonBorder[index]}>
-                {button}
-              </S.Button>
-            </S.Li>
+            <Li key={index}>
+              <Button
+                onClick={() => onClose(button.text)}
+                color={button.color}
+                style={index === 1 ? { borderLeft: '0.5px solid #dbdbdb' } : {}}
+              >
+                {button.text}
+              </Button>
+            </Li>
           );
         })}
-      </S.Ul>
-    </S.AlertSection>
+      </Ul>
+    </AlertSection>
   );
 };
 
 export default AlertModal;
+
+export const AlertSection = styled.div`
+  position: fixed;
+  width: 252px;
+  height: 110px;
+  border-radius: 10px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${({ theme }) => theme.colors.white};
+  z-index: 999;
+  text-align: center;
+  overflow: hidden;
+`;
+
+export const Strong = styled.strong`
+  display: inline-block;
+  border: none;
+  margin: 22px auto;
+  font-size: 16px;
+`;
+
+export const Ul = styled.ul`
+  border-top: ${({ theme }) => `0.5px solid ${theme.colors.gray}`};
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Li = styled.li`
+  flex: 1;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 46px;
+  font-weight: 700;
+  color: ${({ color }) => color || 'inherit'};
+`;
